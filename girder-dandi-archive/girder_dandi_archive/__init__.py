677d3b1322c020abc5c2e73afc33c5ddfe1d0eb7
Michael Grauer <michael.grauer@kitware.com>
2019-10-07 18:36:24 +0000

    Add girder-dandi-archive plugin skeleton

---
+++ girder-dandi-archive/girder_dandi_archive/__init__.py
@@ -0,0 +1,9 @@
+from girder import plugin
+
+
+class GirderPlugin(plugin.GirderPlugin):
+    DISPLAY_NAME = 'DANDI Archive'
+
+    def load(self, info):
+        # add plugin loading logic here
+        pass
