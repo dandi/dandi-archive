d7b0a894f63695ac65244eff88bd791567a3016e
Zach Mullen <zach.mullen@kitware.com>
2019-10-10 20:15:05 -0400

    Vue.nextTick to the rescue

--- web/src/store.js
+++ web/src/store.js
@@ -30,7 +30,12 @@ export default new Vuex.Store({
       if (result._modelType === 'item') {
         const resp = await state.girderRest.get(`folder/${result.folderId}`);
         commit('setBrowseLocation', resp.data);
-        commit('setSelected', [result]);
+        // Because setting the location is going to trigger the DataBrowser to
+        // set its selected value to [], which due to two-way binding also propagates back
+        // up to this component, we must defer this to the next tick so that this runs after that,
+        // as we have no way to update the DataBrowser location without having it also reset the
+        // selection internally.
+        Vue.nextTick(() => { commit('setSelected', [result]); });
       } else {
         commit('setBrowseLocation', result);
       }
