518e15ee97326f047169502a6557053a74f7f9f1
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-02-25 11:50:35 -0500

    Fix missing default value

--- web/src/components/MetaNode.vue
+++ web/src/components/MetaNode.vue
@@ -88,7 +88,7 @@ export default {
   },
   data() {
     return {
-      value: this.copyValue(this.initial),
+      value: this.copyValue(this.initial) || this.defaultInitial(),
       additionalProps: {},
     };
   },
