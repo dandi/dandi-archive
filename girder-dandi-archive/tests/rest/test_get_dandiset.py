27e26449288dea101afbfcc0f6beab136c6944e5
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-03-26 12:35:33 -0400

    Move list dandisets and get dandiset endpoints to be idiomatic
    Move list dandisets to `/api/v1/dandi`
    Move get dandiset to `/api/v1/dandi/<dandiset_id>`

--- girder-dandi-archive/tests/rest/test_get_dandiset.py
+++ girder-dandi-archive/tests/rest/test_get_dandiset.py
@@ -3,41 +3,22 @@ from pytest_girder.assertions import assertStatus, assertStatusOk
 from rest_utils import assert_dandisets_are_equal
 
 pytestmark = pytest.mark.plugin("dandi_archive")
-path = "/dandi"
 
 
 def test_get_dandiset(server, request_auth, dandiset_1):
     identifier = dandiset_1["name"]
 
-    resp = server.request(
-        path=path, method="GET", params={"identifier": identifier}, **request_auth
-    )
+    resp = server.request(path="/dandi/" + identifier, method="GET", **request_auth,)
     assertStatusOk(resp)
 
     assert_dandisets_are_equal(dandiset_1, resp.json)
 
 
 def test_get_dandiset_does_not_exist(server, request_auth):
-    resp = server.request(
-        path=path, method="GET", params={"identifier": "000001"}, **request_auth
-    )
-    assertStatus(resp, 400)
-
-
-def test_get_dandiset_no_identifier(server, request_auth, dandiset_1):
-    resp = server.request(path=path, method="GET", params={}, **request_auth)
-    assertStatus(resp, 400)
-
-
-def test_get_dandiset_empty_identifier(server, request_auth, dandiset_1):
-    resp = server.request(
-        path=path, method="GET", params={"identifier": ""}, **request_auth,
-    )
+    resp = server.request(path="/dandi/000001", method="GET", **request_auth)
     assertStatus(resp, 400)
 
 
 def test_get_dandiset_invalid_identifier(server, request_auth, dandiset_1):
-    resp = server.request(
-        path=path, method="GET", params={"identifier": "1"}, **request_auth
-    )
+    resp = server.request(path="/dandi/x", method="GET", **request_auth)
     assertStatus(resp, 400)
