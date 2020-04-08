805d0c4765552723b078a771d5b269ce9ec6dd9d
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-04-08 11:36:32 -0400

    Remove wrapping v-row in DandisetList

--- web/src/components/DandisetList.vue
+++ web/src/components/DandisetList.vue
@@ -1,66 +1,64 @@
 <template>
-  <v-row>
-    <v-list
-      two-line
-      subheader
+  <v-list
+    two-line
+    subheader
+  >
+    <v-list-item
+      v-for="item in items"
+      :key="item._id"
+      selectable
+      :to="{ name: 'dandisetLanding', params: { id: item._id } }"
     >
-      <v-list-item
-        v-for="item in items"
-        :key="item._id"
-        selectable
-        :to="{ name: 'dandisetLanding', params: { id: item._id } }"
+      <v-row
+        no-gutters
+        align="center"
       >
-        <v-row
-          no-gutters
-          align="center"
-        >
-          <v-col cols="12">
-            <v-list-item-content>
-              <v-list-item-title>
-                <template v-if="item.meta.dandiset.version">
-                  <v-chip
-                    v-if="item.meta.dandiset.version === 'draft'"
-                    small
-                    color="amber lighten-3"
-                    text-color="amber darken-4"
-                  >
-                    <b>DRAFT</b>
-                  </v-chip>
-                  <v-chip
-                    v-else
-                    small
-                    color="light-blue lighten-4"
-                    text-color="light-blue darken-3"
-                  >
-                    <b>{{ item.meta.dandiset.version }}</b>
-                  </v-chip>
-                </template>
-                {{ item.meta.dandiset.name }}
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                Contact <b>{{ getDandisetContact(item) }}</b>
-                ·
-                Created on <b>{{ formatDate(item.created) }}</b>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-col>
-          <!-- TODO: Uncomment this once backend detail issue is sorted -->
-          <!-- <v-col cols="1">
-            <v-icon color="primary">
-              mdi-file
-            </v-icon>
-            {{ item.details.nItems }}
-          </v-col>
-          <v-col cols="1">
-            <v-icon color="primary">
-              mdi-server
-            </v-icon>
-            {{ filesize(item.size) }}
-          </v-col> -->
-        </v-row>
-      </v-list-item>
-    </v-list>
-  </v-row>
+        <v-col cols="12">
+          <v-list-item-content>
+            <v-list-item-title>
+              <template v-if="item.meta.dandiset.version">
+                <v-chip
+                  v-if="item.meta.dandiset.version === 'draft'"
+                  small
+                  color="amber lighten-3"
+                  text-color="amber darken-4"
+                >
+                  <b>DRAFT</b>
+                </v-chip>
+                <v-chip
+                  v-else
+                  small
+                  color="light-blue lighten-4"
+                  text-color="light-blue darken-3"
+                >
+                  <b>{{ item.meta.dandiset.version }}</b>
+                </v-chip>
+              </template>
+              {{ item.meta.dandiset.name }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Contact <b>{{ getDandisetContact(item) }}</b>
+              ·
+              Created on <b>{{ formatDate(item.created) }}</b>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-col>
+        <!-- TODO: Uncomment this once backend detail issue is sorted -->
+        <!-- <v-col cols="1">
+          <v-icon color="primary">
+            mdi-file
+          </v-icon>
+          {{ item.details.nItems }}
+        </v-col>
+        <v-col cols="1">
+          <v-icon color="primary">
+            mdi-server
+          </v-icon>
+          {{ filesize(item.size) }}
+        </v-col> -->
+      </v-row>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
