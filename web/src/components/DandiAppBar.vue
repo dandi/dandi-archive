3406fdcda43a6f6c048c4bb7c36c6600e9534e9f
JellyWang-3 <jelly.wang@kitware.com>
2020-04-13 15:05:06 -0400

    Fixes #261, take out the search bar completely

--- web/src/components/DandiAppBar.vue
+++ web/src/components/DandiAppBar.vue
@@ -76,33 +76,6 @@
       </span>
     </v-tooltip>
     <v-spacer/>
-    <girder-search @select="selectSearchResult" search-mode="dandi"
-        :search-types="['item']" :hide-options-menu="true" placeholder="Type search query" >
-      <template v-slot:searchresult="result">
-        <v-list-item-action>
-          <v-icon> {{ $vuetify.icons.values[result._modelType] }} </v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>{{ result.name }}</v-list-item-title>
-          <v-list-item-subtitle>
-            {{ result.meta.institution + " " + result.meta.lab + " lab" }}
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </template>
-      <template v-slot:noresult="{ searchText }">
-        <v-list-item-action>
-          <v-icon>$vuetify.icons.alert</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>
-            No results found for query '{{ searchText }}'
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Please use our search grammar.
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </template>
-    </girder-search>
     <v-menu offset-y v-if="loggedIn" :close-on-content-click="false">
       <template v-slot:activator="{ on }">
         <v-btn v-on="on" class="ml-2" icon dark>
