1cf67f9c40903389b14f1c380052edf78f2f55bd
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-04-13 11:10:36 -0400

    Replace duplicate code with DandisetPage.vue

--- web/src/views/MyDandisetsView/MyDandisetsView.vue
+++ web/src/views/MyDandisetsView/MyDandisetsView.vue
@@ -1,79 +1,15 @@
 <template>
-  <div>
-    <v-toolbar
-      color="grey darken-2 white--text"
-    >
-      <v-toolbar-title class="d-none d-md-block mx-8">
-        My Dandisets
-      </v-toolbar-title>
-      <v-divider
-        class="d-none d-md-block"
-        vertical
-      />
-      <div class="mx-6">
-        Sort By:
-      </div>
-      <v-chip-group
-        v-model="selection"
-        active-class="white light-blue--text"
-        dark
-        mandatory
-      >
-        <v-chip
-          v-for="option in options"
-          :key="option.name"
-          @click="changeSort({sort: option.sort})"
-        >
-          {{ option.name }}
-        </v-chip>
-      </v-chip-group>
-      <SearchField />
-    </v-toolbar>
-    <DandisetList
-      class="
-        mx-12
-        my-12"
-      :dandisets="dandisets"
-    />
-    <v-pagination
-      v-model="page"
-      :length="pages"
-    />
-  </div>
+  <DandisetPage
+    title="My Dandisets"
+    module="myDandisets"
+  />
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
-
-import DandisetList from '@/components/DandisetList.vue';
-import SearchField from '@/views/PublicDandisetsView/SearchField.vue';
-import { basicDandisetSortingOptions } from '@/utils';
+import DandisetPage from '@/components/DandisetPage.vue';
 
 export default {
   name: 'MyDandisetsView',
-  components: { DandisetList, SearchField },
-  data() {
-    return {
-      selection: 'newest',
-      options: basicDandisetSortingOptions,
-    };
-  },
-  computed: {
-    page: {
-      get() {
-        return this.$store.state.myDandisets.page;
-      },
-      set(page) {
-        this.changePage({ page });
-      },
-    },
-    ...mapState('myDandisets', ['dandisets', 'pages']),
-  },
-  created() {
-    this.reload();
-  },
-  methods: {
-    ...mapActions('myDandisets', ['changeSort', 'changePage', 'reload']),
-  },
+  components: { DandisetPage },
 };
 </script>
--- web/src/views/PublicDandisetsView/PublicDandisetsView.vue
+++ web/src/views/PublicDandisetsView/PublicDandisetsView.vue
@@ -1,79 +1,15 @@
 <template>
-  <div>
-    <v-toolbar
-      color="grey darken-2 white--text"
-    >
-      <v-toolbar-title class="d-none d-md-block mx-8">
-        Public Dandisets
-      </v-toolbar-title>
-      <v-divider
-        class="d-none d-md-block"
-        vertical
-      />
-      <div class="mx-6">
-        Sort By:
-      </div>
-      <v-chip-group
-        v-model="selection"
-        active-class="white light-blue--text"
-        dark
-        mandatory
-      >
-        <v-chip
-          v-for="option in options"
-          :key="option.name"
-          @click="changeSort({sort: option.sort})"
-        >
-          {{ option.name }}
-        </v-chip>
-      </v-chip-group>
-      <SearchField />
-    </v-toolbar>
-    <DandisetList
-      class="
-        mx-12
-        my-12"
-      :dandisets="dandisets"
-    />
-    <v-pagination
-      v-model="page"
-      :length="pages"
-    />
-  </div>
+  <DandisetPage
+    title="Public Dandisets"
+    module="publicDandisets"
+  />
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
-
-import DandisetList from '@/components/DandisetList.vue';
-import SearchField from '@/views/PublicDandisetsView/SearchField.vue';
-import { basicDandisetSortingOptions } from '@/utils';
+import DandisetPage from '@/components/DandisetPage.vue';
 
 export default {
   name: 'PublicDandisetsView',
-  components: { DandisetList, SearchField },
-  data() {
-    return {
-      selection: 'newest',
-      options: basicDandisetSortingOptions,
-    };
-  },
-  computed: {
-    page: {
-      get() {
-        return this.$store.state.publicDandisets.page;
-      },
-      set(page) {
-        this.changePage({ page });
-      },
-    },
-    ...mapState('publicDandisets', ['dandisets', 'pages']),
-  },
-  created() {
-    this.reload();
-  },
-  methods: {
-    ...mapActions('publicDandisets', ['changeSort', 'changePage', 'reload']),
-  },
+  components: { DandisetPage },
 };
 </script>
