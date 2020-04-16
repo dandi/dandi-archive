33115eb57b12184a60f3b95367deba4db4ed06b1
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-04-16 10:08:48 -0400

    Add user registration page (#260)
    * Add user registration page

--- web/src/views/UserRegisterView/UserRegisterView.vue
+++ web/src/views/UserRegisterView/UserRegisterView.vue
@@ -1,11 +1,39 @@
 <template>
   <v-container>
-    TODO: UserRegisterView
+    <GirderRegister :oauth-providers="oauthProviders" />
   </v-container>
 </template>
 
 <script>
+import GirderRegister from '@girder/components/src/components/Authentication/Register.vue';
+import { OauthTokenPrefix, OauthTokenSuffix } from '@girder/components/src/rest';
+import girderRest, { loggedIn } from '@/rest';
+
 export default {
   name: 'UserRegisterView',
+  components: {
+    GirderRegister,
+  },
+  computed: {
+    loggedIn,
+  },
+  asyncComputed: {
+    async oauthProviders() {
+      const { data } = await girderRest.get('oauth/provider', {
+        params: {
+          redirect: `${window.location.href}${OauthTokenPrefix}{girderToken}${OauthTokenSuffix}`,
+          list: true,
+        },
+      });
+      return data;
+    },
+  },
+  watch: {
+    loggedIn(val) {
+      if (val) {
+        this.$router.push({ name: 'home' });
+      }
+    },
+  },
 };
 </script>
