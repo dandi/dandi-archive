7d0f74b5cc05a959b767f80581b75607f3e27d06
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-04-30 11:35:46 -0400

    Move search icon to beginning of search bar

--- web/src/components/DandisetSearchField.vue
+++ web/src/components/DandisetSearchField.vue
@@ -11,7 +11,7 @@
       color="black"
       @input="updateSearch"
     >
-      <template v-slot:append>
+      <template v-slot:prepend-inner>
         <v-icon @click="performSearch">
           mdi-magnify
         </v-icon>
