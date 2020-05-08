eb1ed383f7d24d6b114f8d8038d3ce67df2da57b
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-05-08 15:25:46 -0400

    Use builtin django permissions

--- publish/api.py
+++ publish/api.py
@@ -1,21 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from .models import Dandiset, NWBFile, Subject
 from .serializers import DandisetSerializer, DandisetPublishSerializer, NWBFileSerializer, SubjectSerializer
 from .tasks import publish_dandiset
-
-
-class PermissionMixin:
-    def get_permissions(self):
-        if self.action in ['retrieve', 'list']:
-            permission_classes = []
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
 
 
 class DandisetPagination(PageNumberPagination):
@@ -24,10 +15,11 @@ class DandisetPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
 
 
-class DandisetViewSet(PermissionMixin, viewsets.ModelViewSet):
+class DandisetViewSet(viewsets.ModelViewSet):
     queryset = Dandiset.objects.all().order_by('id')
     serializer_class = DandisetSerializer
     pagination_class = DandisetPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     @action(detail=False, methods=['POST'])
     def publish(self, request, *args, **kwargs):
@@ -37,13 +29,15 @@ class DandisetViewSet(PermissionMixin, viewsets.ModelViewSet):
         return Response(publish_dandiset.delay(**data).id)
 
 
-class SubjectViewSet(PermissionMixin, viewsets.ModelViewSet):
+class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all().order_by('id')
     serializer_class = SubjectSerializer
     pagination_class = DandisetPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class NWBFileViewSet(PermissionMixin, viewsets.ModelViewSet):
+class NWBFileViewSet(viewsets.ModelViewSet):
     queryset = NWBFile.objects.all().order_by('id')
     serializer_class = NWBFileSerializer
     pagination_class = DandisetPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
