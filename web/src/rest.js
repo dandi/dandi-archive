2abc516b6274bcdb3bfe7851927b9fcf7a9a2543
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-05-18 17:18:20 -0400

    Remove VUE_APP_API_ROOT for consistency

--- web/src/rest.js
+++ web/src/rest.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { RestClient } from '@girder/components/src';
 
-const apiRoot = process.env.VUE_APP_API_ROOT || 'http://localhost:8080/api/v1';
+const apiRoot = process.env.VUE_APP_API_ROOT;
 const publishApiRoot = process.env.VUE_APP_PUBLISH_API_ROOT;
 
 const girderRest = new RestClient({ apiRoot, setLocalCookie: true });
