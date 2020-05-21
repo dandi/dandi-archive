ea4d08de74cb0d6ed8e49b1eee4ca42dfb3f443c
Brian Helba <brian.helba@kitware.com>
2020-05-21 10:52:26 -0400

    Fix incorrect env vars

--- dev/.env.docker-compose-native.sh
+++ dev/.env.docker-compose-native.sh
@@ -4,9 +4,9 @@ DJANGO_CONFIGURATION=DevelopmentConfiguration
 DJANGO_DATABASE_URL=postgres://postgres:postgres@localhost:5432/dandi
 DJANGO_CELERY_BROKER_URL=amqp://localhost:5672/
 DJANGO_MINIO_STORAGE_ENDPOINT=localhost:9000
-DJANGO_MINIO_STORAGE_ACCESS_KEY=minioAdminAccessKey
-DJANGO_MINIO_STORAGE_SECRET_KEY=minioAdminSecretKey
+DJANGO_MINIO_STORAGE_ACCESS_KEY=djangoAccessKey
+DJANGO_MINIO_STORAGE_SECRET_KEY=djangoSecretKey
 DJANGO_STORAGE_BUCKET_NAME=dandi-files
-DJANGO_DANDISETS_BUCKET_NAME=dandi-dandisets
+DJANGO_DANDI_DANDISETS_BUCKET_NAME=dandi-dandisets
 '
 for var in $vars; do export "${var}"; done
