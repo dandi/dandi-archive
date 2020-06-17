18c76e971a3778eb1f585541078b1f9470356fdb
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-06-17 12:22:08 -0400

    Extremely rudimentary publish file browser working

--- web/src/views/FileBrowserView/PublishFileBrowser.vue
+++ web/src/views/FileBrowserView/PublishFileBrowser.vue
@@ -1,105 +1,95 @@
 <template>
   <v-container>
-    <v-row v-if="selected">
-      <v-col :cols="selected.length ? 8 : 12">
-        <girder-file-manager
-          ref="girderFileManager"
-          selectable
-          root-location-disabled
-          :location.sync="location"
-          :value="selected"
-          :initial-items-per-page="25"
-          :items-per-page-options="[10,25,50,100,-1]"
-          @input="setSelected"
+    <v-row>
+      <v-col :cols="12">
+        <v-data-table
+          :items="items"
+          :headers="headers"
+          hide-default-header
+          item-key="name"
+          selectable-key="folder"
+          @click:row="selectPath"
         >
-          <template
-            v-if="isDandiset"
-            v-slot:headerwidget
-          >
-            <v-btn
-              icon
-              color="primary"
-              :to="{ name: 'dandisetLanding', params: { identifier: location.meta.dandiset.identifier }}"
-            >
-              <v-icon>mdi-eye</v-icon>
-            </v-btn>
+          <template v-slot:header>
+            {{ location }}
           </template>
-        </girder-file-manager>
-      </v-col>
-      <v-col
-        v-if="selected.length"
-        cols="4"
-      >
-        <girder-data-details
-          :value="selected"
-          :action-keys="actions"
-          @action="handleAction"
-        />
+
+        <!-- <template v-slot:item="{ item }">
+          <v-row>
+            <v-icon>mdi-folder</v-icon>
+            <span>{{ item.name }}</span>
+          </v-row>
+        </template> -->
+        </v-data-table>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import {
-  mapGetters,
-  mapMutations,
-  mapState,
-  mapActions,
-} from 'vuex';
-import { FileManager as GirderFileManager } from '@girder/components/src/components/Snippet';
+import { mapState } from 'vuex';
+import { publishRest } from '@/rest';
 
-import {
-  getLocationFromRoute,
-} from '@/utils';
-
+const isFolder = (pathName) => (pathName.slice(-1) === '/');
+const parentDirectory = '..';
 
 export default {
   name: 'PublishFileBrowser',
-  components: { GirderFileManager },
+  props: {
+    identifier: {
+      type: String,
+      required: true,
+    },
+    version: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      location: '/',
+      // items: [],
+      headers: [
+        {
+          text: 'Name',
+          value: 'name',
+          align: 'start',
+        },
+      ],
+    };
+  },
   computed: {
-    isDandiset() {
-      return !!(this.location && this.location.meta && this.location.meta.dandiset);
-    },
-    location: {
-      get() {
-        return this.$store.state.girder.browseLocation;
-      },
-      set(value) {
-        this.setBrowseLocation(value);
-      },
-    },
-    ...mapState('girder', ['selected']),
-    ...mapGetters('girder', ['loggedIn']),
+    ...mapState('dandiset', ['publishDandiset']),
   },
-  watch: {
-    location(newValue) {
-      const { _modelType, _id } = this.$route.params;
+  asyncComputed: {
+    items: {
+      async get() {
+        const { version, identifier, location } = this;
+        const { data } = await publishRest.get(`dandisets/${identifier}/versions/${version}/assets/paths/`, {
+          params: {
+            path_prefix: location,
+          },
+        });
 
-      if (newValue._modelType !== _modelType || newValue._id !== _id) {
-        this.$router.push({ name: 'file-browser', params: { _modelType: newValue._modelType, _id: newValue._id } });
-      }
+        const mapped = data.map((x) => ({ name: x, folder: isFolder(x) }));
+        return [
+          { name: parentDirectory, folder: true },
+          ...mapped,
+        ];
+      },
+      default: [],
     },
   },
-  created() {
-    const location = getLocationFromRoute(this.$route);
-    this.setBrowseLocation(location);
-    this.fetchFullLocation(location);
-  },
+  watch: {},
+  created() {},
   methods: {
-    async handleAction(action) {
-      if (action.name === 'Delete') {
-        await this.$refs.girderFileManager.refresh();
-        this.setSelected([]);
+    selectPath({ name }) {
+      if (name === parentDirectory) {
+        this.location = `${this.location.split('/').slice(0, -2).join('/')}/`;
+      } else {
+        this.location = `${this.location}${name}`;
       }
     },
-    ...mapMutations('girder', ['setBrowseLocation', 'setSelected']),
-    ...mapActions('girder', ['fetchFullLocation']),
   },
 };
 </script>
-<style>
-.girder-file-browser .secondary .row .spacer {
-  display: none;
-}
-</style>
