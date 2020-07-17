a9c756862b158d78afb477d874850d808776d77a
Brian Helba <brian.helba@kitware.com>
2020-07-17 10:22:33 -0400

    Move requirements.txt to setup.py

---
+++ setup.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+readme_file = Path(__file__).parent / 'README.md'
+if readme_file.exists():
+    with readme_file.open() as f:
+        long_description = f.read()
+else:
+    # WHen this is first installed in development Docker, README.md is not available
+    long_description = ''
+
+setup(
+    name='dandi-archive',
+    version='0.1.0',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache 2.0',
+    author='Kitware, Inc',
+    author_email='kitware@kitware.com',
+    keywords='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python',
+    ],
+    python_requires='>=3.8',
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'celery',
+        'dandi==0.4.5',
+        'django',
+        'django-admin-display',
+        'django-configurations[database,email]',
+        'django-cors-headers',
+        'django-extensions',
+        'django-filter',
+        'django-girders',
+        'djangorestframework',
+        'drf-extensions',
+        'drf-yasg',
+        'httpx',
+        'psycopg2',
+        'pyyaml',
+        'rich',
+        'whitenoise[brotli]',
+        # Production-only
+        'django-storages',
+        'gunicorn',
+        'sentry-sdk',
+        # Development-only
+        'django-debug-toolbar',
+        'django-minio-storage',
+        'ipython',
+    ],
+)
