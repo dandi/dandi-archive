7117a04d5ec021fc59e39c25f3d6539e39fce5b6
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2020-08-03 11:51:59 -0400

    remove nonstring metadata test

--- publish/tests/test_version.py
+++ publish/tests/test_version.py
@@ -32,18 +32,12 @@ def test_version_from_girder(dandiset_factory, mock_girder_client):
 
 
 @pytest.mark.django_db
-def test_version_from_girder_no_metadata(dandiset_factory, mock_girder_client):
-    dandiset = dandiset_factory()
+def test_version_from_girder_no_metadata(dandiset, mock_girder_client):
+    # this test relies on DandisetFactory producing a dandiset with a draft_folder_id
+    # that is considered invalid when used with Version.from_girder and the mocked
+    # girder client.
     with pytest.raises(ValidationError, match='has no "meta" field.'):
         Version.from_girder(dandiset, mock_girder_client)
-
-
-@pytest.mark.django_db
-def test_version_from_girder_nonstring_metadata(dandiset_factory, mock_girder_client):
-    dandiset = dandiset_factory(draft_folder_id='nonstring_meta_folder_id')
-    version = Version.from_girder(dandiset, mock_girder_client)
-    assert version.name is not None
-    assert version.description is not None
 
 
 @pytest.mark.django_db
