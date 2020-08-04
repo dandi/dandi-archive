9a4274e0aeb5fab098e46f1e797bb2d69a7f9b5c
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-08-04 18:01:00 -0400

    Remove generic girder registration
    Remove the GWC registration component and only support Github OAuth
    registration.

--- web/src/views/UserRegisterView/UserRegisterView.vue
+++ web/src/views/UserRegisterView/UserRegisterView.vue
@@ -1,18 +1,41 @@
 <template>
   <v-container v-page-title="'Sign Up'">
-    <GirderRegister :oauth-providers="oauthProviders" />
+    <h4>Register with Github</h4>
+    <v-btn
+      v-for="provider in oauthProviders"
+      :key="provider.id"
+      :dark="iconMap[provider.id].dark"
+      :color="iconMap[provider.id].color"
+      :href="provider.url"
+      class="ml-0 mr-3"
+    >
+      <v-icon left="left">
+        {{ $vuetify.icons.values[iconMap[provider.id].icon] }}
+      </v-icon>
+      {{ provider.name }}
+    </v-btn>
   </v-container>
 </template>
 
 <script>
-import GirderRegister from '@girder/components/src/components/Authentication/Register.vue';
 import { OauthTokenPrefix, OauthTokenSuffix } from '@girder/components/src/rest';
 import girderRest, { loggedIn } from '@/rest';
 
+// Copied from https://github.com/girder/girder_web_components Authentication/OAuth.vue
+const iconMap = {
+  github: { dark: true, icon: 'github' },
+  google: { dark: true, icon: 'google', color: '#3367d6' },
+  linkedin: { dark: true, icon: 'linkedin', color: '#283e4a' },
+  bitbucket: { dark: false, icon: 'bitbucket' },
+  box: { dark: true, icon: 'box_com', color: '#0071f7' },
+  globus: { dark: true, icon: 'globus', color: '#335a95' },
+};
 export default {
   name: 'UserRegisterView',
-  components: {
-    GirderRegister,
+  data() {
+    return {
+      iconMap,
+    };
   },
   computed: {
     loggedIn,
