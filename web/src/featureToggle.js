d744a0a45313b3ee2d1e0f17d0ac6641ae38a32a
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-08-14 10:15:34 -0400

    Clean up toggle definitions

--- web/src/featureToggle.js
+++ web/src/featureToggle.js
@@ -2,37 +2,34 @@
 /* eslint no-throw-literal: 0 */
 import Vue from 'vue';
 
-// Define all feature toggles here
-// They can also be enabled/disabled here for development
-const TOGGLES = {
-  UNIFIED_API: false,
-};
-
 // Create a component to hold the feature toggles reactively
 // This object can be mutated at runtime to enable/disable feature toggles
 const featureToggles = new Vue({
   data() {
-    return TOGGLES;
+    return {
+      UNIFIED_API: false,
+    };
   },
 });
 
-// Generate the computed prop to use in the mixin
-const computed = Object.keys(TOGGLES).reduce((result, toggle) => {
-  // eslint-disable-next-line no-param-reassign
-  result[toggle] = () => featureToggles[toggle];
-  return result;
-}, {});
-
 // Register the feature toggles as computed properties on all components
 // Now feature toggles are available on all components and templates without any imports
-Vue.mixin({ computed });
+Vue.mixin({
+  computed: {
+    UNIFIED_API() {
+      return featureToggles.UNIFIED_API;
+    },
+  },
+});
+
+export default featureToggles;
 
 // We also want to support toggling feature toggles in branch previews and in master
 // These functions are made available globally so they can be called from the console
 
 /** Enables a feature toggle. Only usable from the console. */
 function enable(featureToggle) {
-  if (!(featureToggle in TOGGLES)) {
+  if (!(featureToggle in featureToggles)) {
     throw `Feature toggle ${featureToggle} is not defined`;
   }
   featureToggles[featureToggle] = true;
@@ -41,7 +38,7 @@ function enable(featureToggle) {
 
 /** Disables a feature toggle. Only usable from the console. */
 function disable(featureToggle) {
-  if (!(featureToggle in TOGGLES)) {
+  if (!(featureToggle in featureToggles)) {
     throw `Feature toggle ${featureToggle} is not defined`;
   }
   featureToggles[featureToggle] = false;
