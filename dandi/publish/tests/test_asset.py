0dc4a0fbbc9fd0e97f5d07a89686624eda548417
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2020-08-14 14:59:36 -0400

    add test for get_path method

--- dandi/publish/tests/test_asset.py
+++ dandi/publish/tests/test_asset.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from dandi.publish.models import Asset
@@ -73,3 +75,18 @@ def test_asset_rest_retrieve(api_client, asset):
         'updated': TIMESTAMP_RE,
         'metadata': asset.metadata,
     }
+
+
+@pytest.mark.django_db
+def test_asset_path(api_client, asset):
+    path = os.path.split(asset.path)
+    asset_directory = path[: len(path) - 1]
+    asset_filename = path[len(path) - 1]
+
+    res = api_client.get(
+        f'/api/dandisets/{asset.version.dandiset.identifier}/'
+        f'versions/{asset.version.version}/assets/paths/',
+        {'path_prefix': asset_directory},
+    )
+
+    assert asset_filename in res.data
