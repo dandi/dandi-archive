a26f84e59c608c5c6c43bd3139de85af86202828
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-08-24 17:34:32 -0400

    Tweak test_stats factoryboy fixtures and assert ordering

--- dandi/publish/tests/test_stats.py
+++ dandi/publish/tests/test_stats.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.django_db
 def test_stats_none(api_client):
     resp = api_client.get('/api/stats/')
-    assert {
+    assert resp.json() == {
         'draft_count': 0,
         'published_count': 0,
         'user_count': 0,
@@ -12,14 +12,13 @@ def test_stats_none(api_client):
         'subject_count': 0,
         'cell_count': 0,
         'size': 0,
-    } == resp.json()
+    }
 
 
 @pytest.mark.django_db
-def test_stats_draft_count(api_client, draft_version_factory):
-    draft_version_factory()
+def test_stats_draft_count(api_client, draft_version):
     resp = api_client.get('/api/stats/')
-    assert {
+    assert resp.json() == {
         'draft_count': 1,
         'published_count': 0,
         'user_count': 0,
@@ -27,14 +26,13 @@ def test_stats_draft_count(api_client, draft_version_factory):
         'subject_count': 0,
         'cell_count': 0,
         'size': 0,
-    } == resp.json()
+    }
 
 
 @pytest.mark.django_db
-def test_stats_published_count(api_client, version_factory):
-    version_factory()
+def test_stats_published_count(api_client, version):
     resp = api_client.get('/api/stats/')
-    assert {
+    assert resp.json() == {
         'draft_count': 1,
         'published_count': 1,
         'user_count': 0,
@@ -42,14 +40,13 @@ def test_stats_published_count(api_client, version_factory):
         'subject_count': 0,
         'cell_count': 0,
         'size': 0,
-    } == resp.json()
+    }
 
 
 @pytest.mark.django_db
-def test_stats_user_count(api_client, user_factory):
-    user_factory()
+def test_stats_user_count(api_client, user):
     resp = api_client.get('/api/stats/')
-    assert {
+    assert resp.json() == {
         'draft_count': 0,
         'published_count': 0,
         'user_count': 1,
@@ -57,14 +54,13 @@ def test_stats_user_count(api_client, user_factory):
         'subject_count': 0,
         'cell_count': 0,
         'size': 0,
-    } == resp.json()
+    }
 
 
 @pytest.mark.django_db
-def test_stats_size(api_client, asset_factory):
-    asset = asset_factory()
+def test_stats_size(api_client, asset):
     resp = api_client.get('/api/stats/')
-    assert {
+    assert resp.json() == {
         'draft_count': 1,
         'published_count': 1,
         'user_count': 0,
@@ -72,4 +68,4 @@ def test_stats_size(api_client, asset_factory):
         'subject_count': 0,
         'cell_count': 0,
         'size': asset.size,
-    } == resp.json()
+    }
