cab911e971a8fd171a478096e79a4824c9c3d1ac
Brian Helba <brian.helba@kitware.com>
2020-08-28 14:45:22 -0400

    Rename some stats
    published_count is specifically a count of dandisets with published versions,
    so published_dandiset_count is a better name.
    
    Under normal operation, every dandiset has a existant draft version, so it's
    more straightforward to directly report the total count of dandisets.

--- dandi/publish/tests/test_stats.py
+++ dandi/publish/tests/test_stats.py
@@ -4,27 +4,27 @@ import pytest
 @pytest.mark.django_db
 def test_stats_baseline(api_client):
     assert api_client.get('/api/stats/').data == {
-        'draft_count': 0,
-        'published_count': 0,
+        'dandiset_count': 0,
+        'published_dandiset_count': 0,
         'user_count': 0,
         'size': 0,
     }
 
 
 @pytest.mark.django_db
-def test_stats_draft(api_client, draft_version):
+def test_stats_draft(api_client, dandiset):
     stats = api_client.get('/api/stats/').data
 
-    assert stats['draft_count'] == 1
-    assert stats['published_count'] == 0
+    assert stats['dandiset_count'] == 1
+    assert stats['published_dandiset_count'] == 0
 
 
 @pytest.mark.django_db
 def test_stats_published(api_client, version):
     stats = api_client.get('/api/stats/').data
 
-    assert stats['draft_count'] == 1
-    assert stats['published_count'] == 1
+    assert stats['dandiset_count'] == 1
+    assert stats['published_dandiset_count'] == 1
 
 
 @pytest.mark.django_db
--- dandi/publish/views/stats.py
+++ dandi/publish/views/stats.py
@@ -2,19 +2,19 @@ from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from dandi.publish.models import Asset, Dandiset, DraftVersion
+from dandi.publish.models import Asset, Dandiset
 
 
 @api_view()
 def stats_view(self):
-    draft_count = DraftVersion.objects.count()
-    published_count = Dandiset.published_count()
+    dandiset_count = Dandiset.objects.count()
+    published_dandiset_count = Dandiset.published_count()
     user_count = User.objects.count()
     size = Asset.total_size()
     return Response(
         {
-            'draft_count': draft_count,
-            'published_count': published_count,
+            'dandiset_count': dandiset_count,
+            'published_dandiset_count': published_dandiset_count,
             'user_count': user_count,
             'size': size,
         }
