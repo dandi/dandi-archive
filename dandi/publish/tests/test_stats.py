57574ecb222e264b4d102d4a1030086bde1c06a8
Brian Helba <brian.helba@kitware.com>
2020-08-28 11:29:56 -0400

    Refactor stats tests for more limited, but scalable assertions

--- dandi/publish/tests/test_stats.py
+++ dandi/publish/tests/test_stats.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_stats_none(api_client):
+def test_stats_baseline(api_client):
     assert api_client.get('/api/stats/').data == {
         'draft_count': 0,
         'published_count': 0,
@@ -12,40 +12,30 @@ def test_stats_none(api_client):
 
 
 @pytest.mark.django_db
-def test_stats_draft_count(api_client, draft_version):
-    assert api_client.get('/api/stats/').data == {
-        'draft_count': 1,
-        'published_count': 0,
-        'user_count': 0,
-        'size': 0,
-    }
+def test_stats_draft(api_client, draft_version):
+    stats = api_client.get('/api/stats/').data
+
+    assert stats['draft_count'] == 1
+    assert stats['published_count'] == 0
 
 
 @pytest.mark.django_db
-def test_stats_published_count(api_client, version):
-    assert api_client.get('/api/stats/').data == {
-        'draft_count': 1,
-        'published_count': 1,
-        'user_count': 0,
-        'size': 0,
-    }
+def test_stats_published(api_client, version):
+    stats = api_client.get('/api/stats/').data
+
+    assert stats['draft_count'] == 1
+    assert stats['published_count'] == 1
 
 
 @pytest.mark.django_db
-def test_stats_user_count(api_client, user):
-    assert api_client.get('/api/stats/').data == {
-        'draft_count': 0,
-        'published_count': 0,
-        'user_count': 1,
-        'size': 0,
-    }
+def test_stats_user(api_client, user):
+    stats = api_client.get('/api/stats/').data
+
+    assert stats['user_count'] == 1
 
 
 @pytest.mark.django_db
-def test_stats_size(api_client, asset):
-    assert api_client.get('/api/stats/').data == {
-        'draft_count': 1,
-        'published_count': 1,
-        'user_count': 0,
-        'size': asset.size,
-    }
+def test_stats_asset(api_client, asset):
+    stats = api_client.get('/api/stats/').data
+
+    assert stats['size'] == asset.size
