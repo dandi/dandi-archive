1d8c020dd1bb8c7ef9366cc1fbe3bb8e36752b1c
Brian Helba <brian.helba@kitware.com>
2020-08-28 14:41:33 -0400

    Move published_count to Dandiset and add a test

--- dandi/publish/views/stats.py
+++ dandi/publish/views/stats.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from dandi.publish.models import Asset, DraftVersion, Version
+from dandi.publish.models import Asset, Dandiset, DraftVersion
 
 
 @api_view()
 def stats_view(self):
     draft_count = DraftVersion.objects.count()
-    published_count = Version.published_count()
+    published_count = Dandiset.published_count()
     user_count = User.objects.count()
     size = Asset.total_size()
     return Response(
