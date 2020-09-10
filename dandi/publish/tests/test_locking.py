7d87e5bc766f3d93928488c3f89cee46f30a03fa
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-09-10 11:32:34 -0400

    Add locking tests

---
+++ dandi/publish/tests/test_locking.py
@@ -0,0 +1,81 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_lock(draft_version, user):
+    assert not draft_version.locked
+    assert draft_version.locked_by is None
+
+    draft_version.lock(user)
+
+    assert draft_version.locked
+    assert draft_version.locked_by == user
+
+
+@pytest.mark.django_db
+def test_lock_twice(draft_version, user):
+    draft_version.lock(user)
+
+    with pytest.raises(Exception, match='Draft is locked'):
+        draft_version.lock(user)
+
+
+@pytest.mark.django_db
+def test_lock_other_user(draft_version, user_factory):
+    draft_version.lock(user_factory())
+
+    with pytest.raises(Exception, match='Draft is locked'):
+        draft_version.lock(user_factory())
+
+
+@pytest.mark.django_db
+def test_unlock(draft_version, user):
+    draft_version.lock(user)
+
+    draft_version.unlock(user)
+
+    assert not draft_version.locked
+    assert draft_version.locked_by is None
+
+
+@pytest.mark.django_db
+def test_unlock_unlocked(draft_version, user):
+    with pytest.raises(Exception, match='Cannot unlock a draft that is not locked'):
+        draft_version.unlock(user)
+
+
+@pytest.mark.django_db
+def test_unlock_other_user(draft_version, user_factory):
+    draft_version.lock(user_factory())
+
+    with pytest.raises(Exception, match='Cannot unlock a draft locked by another user'):
+        draft_version.unlock(user_factory())
+
+
+# API Tests
+
+
+@pytest.mark.django_db
+def test_lock_rest(api_client, dandiset, user):
+    api_client.force_authenticate(user=user)
+
+    resp = api_client.post(f'/api/dandisets/{dandiset.identifier}/draft/lock/').data
+
+    print(resp)
+    assert resp['locked']
+    assert resp['locked_by'] == {
+        'email': user.email,
+        'username': user.username,
+    }
+
+
+@pytest.mark.django_db
+def test_unlock_rest(api_client, dandiset, user):
+    dandiset.draft_version.lock(user)
+    api_client.force_authenticate(user=user)
+
+    resp = api_client.post(f'/api/dandisets/{dandiset.identifier}/draft/unlock/').data
+
+    print(resp)
+    assert not resp['locked']
+    assert resp['locked_by'] is None
