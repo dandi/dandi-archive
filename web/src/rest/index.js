859e8e9f5a78a237ac26a5327737ff1260d70fbb
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-09-11 18:24:03 -0400

    Simplify rest loggedIn definition
    Co-authored-by: Jacob Nesbitt <jjnesbitt2@gmail.com>

--- web/src/rest/index.js
+++ web/src/rest/index.js
@@ -2,8 +2,8 @@ import girderRest from '@/rest/girder';
 import publishRest from '@/rest/publish';
 import toggles from '@/featureToggle';
 
-const loggedIn = () => ((toggles.UNIFIED_API) ? !!publishRest.user : !!girderRest.user);
 const user = () => ((toggles.UNIFIED_API) ? publishRest.user : girderRest.user);
+const loggedIn = () => !!user();
 
 export {
   girderRest,
