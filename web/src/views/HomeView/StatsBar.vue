64c07887962b96f9c08524740236ad05e18cd00e
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-09-17 17:07:08 -0400

    Make stats use girder directly instead of vuex

--- web/src/views/HomeView/StatsBar.vue
+++ web/src/views/HomeView/StatsBar.vue
@@ -31,22 +31,43 @@
 </template>
 
 <script>
+import { girderRest } from '@/rest';
 import filesize from 'filesize';
-import { mapState } from 'vuex';
 import SingleStat from '@/views/HomeView/SingleStat.vue';
 
 export default {
   name: 'StatsBar',
   components: { SingleStat },
-  computed: mapState({
-    stats: (state) => [
-      { name: 'dandisets', value: state.stats.drafts, description: 'A DANDI dataset including files and dataset-level metadata' },
-      { name: 'users', value: state.stats.users },
-      { name: 'species', value: state.stats.species },
-      { name: 'subjects', value: state.stats.subjects },
-      { name: 'cells', value: state.stats.cells },
-      { name: 'total data size', value: filesize(state.stats.size, { round: 0 }) },
-    ],
-  }),
+  data() {
+    return {
+      dandisets: 0,
+      users: 0,
+      species: 0,
+      subjects: 0,
+      cells: 0,
+      size: 0,
+    };
+  },
+  computed: {
+    stats() {
+      return [
+        { name: 'dandisets', value: this.dandisets, description: 'A DANDI dataset including files and dataset-level metadata' },
+        { name: 'users', value: this.users },
+        { name: 'species', value: this.species },
+        { name: 'subjects', value: this.subjects },
+        { name: 'cells', value: this.cells },
+        { name: 'total data size', value: filesize(this.size, { round: 0 }) },
+      ]
+    },
+  },
+  async created() {
+    const { data } = await girderRest.get('dandi/stats');
+    this.dandisets = data.draft_count;
+    this.users = data.user_count;
+    this.species = data.species_count;
+    this.subjects = data.subject_count;
+    this.cells = data.cell_count;
+    this.size = data.size;
+  }
 };
 </script>
