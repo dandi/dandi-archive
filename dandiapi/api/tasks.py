d67422ec18eaee2a063021f036ccfa74564583eb
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-11-20 10:04:28 -0500

    Add sha256 checksum validation to validate task

--- dandiapi/api/tasks.py
+++ dandiapi/api/tasks.py
@@ -1,3 +1,7 @@
+import hashlib
+import sys
+from urllib.request import urlopen
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.db.transaction import atomic
@@ -7,22 +11,54 @@ from dandiapi.api.models import AssetBlob, Validation
 logger = get_task_logger(__name__)
 
 
+class ChecksumMismatch(Exception):
+    def __init__(self, expected_sha256, actual_sha256):
+        self.expected_sha256 = expected_sha256
+        self.actual_sha256 = actual_sha256
+
+    def __str__(self):
+        return f'Given checksum {self.expected_sha256} did not match calculated checksum {self.actual_sha256}.'
+
+
 @shared_task
 @atomic
 def validate(validation_id: int) -> None:
     validation: Validation = Validation.objects.get(pk=validation_id)
-    # TODO: Verify sha256 checksum
-    # TODO: Run dandi-cli validation
 
-    # For now we just wait 10 seconds to simulate some latency
-    import time
+    try:
+        BUFFER_SIZE = 4096
+        h = hashlib.sha256()
 
-    time.sleep(10)
+        with urlopen(validation.blob.url) as stream:
+            # html = f.read().decode('utf-8')
+            buffer = stream.read(BUFFER_SIZE)
+            while buffer != b'':
+                h.update(buffer)
+                buffer = stream.read(BUFFER_SIZE)
 
-    validation.state = Validation.State.SUCCEEDED
-    validation.save()
+        sha256 = h.hexdigest()
+        logger.info('Calculated sha256 %s', sha256)
+        if sha256 != validation.sha256:
+            raise ChecksumMismatch(validation.sha256, sha256)
 
-    # TODO separate storages for Validations and Assets require a copy at this point
-    asset_blob, created = AssetBlob.from_validation(validation)
-    if created:
-        asset_blob.save()
+        # TODO: Run dandi-cli validation
+
+        validation.state = Validation.State.SUCCEEDED
+        validation.error = None
+        validation.save()
+
+        # TODO separate storages for Validations and Assets require a copy at this point
+        asset_blob, created = AssetBlob.from_validation(validation)
+        if created:
+            asset_blob.save()
+    except ChecksumMismatch as e:
+        logger.info('Checksum mismatch: %s', str(e))
+        validation.state = Validation.State.FAILED
+        validation.error = str(e)
+        validation.save()
+    except Exception as e:
+        validation.state = Validation.State.FAILED
+        validation.error = 'Internal error'
+        validation.save()
+        # TODO: Can celery recover from a task error?
+        raise e
