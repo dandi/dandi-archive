932639613c14d4eb2b549402267ac70e7500ca1d
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-01-01 15:45:44 -0500

    Update featureToggles.ts to use composition API

--- web/src/featureToggle.ts
+++ web/src/featureToggle.ts
@@ -1,19 +1,29 @@
 /* eslint no-console: 0 */
-/* eslint no-throw-literal: 0 */
 import Vue from 'vue';
+import { reactive } from '@vue/composition-api';
+
+// Declare additions to the global `window` object
+declare global {
+  interface Window {
+    enable: (toggle: string) => void,
+    disable: (toggle: string) => void,
+  }
+}
 
 // Create a component to hold the feature toggles reactively
 // This object can be mutated at runtime to enable/disable feature toggles
-const featureToggles = new Vue({
-  data() {
-    return {
-      DJANGO_API: false,
-    };
-  },
+const featureToggles = reactive({
+  DJANGO_API: false,
 });
+
+// Used to narrow string types
+function isToggle(toggle: string): toggle is keyof (typeof featureToggles) {
+  return Object.keys(featureToggles).includes(toggle);
+}
 
 // Register the feature toggles as computed properties on all components
 // Now feature toggles are available on all components and templates without any imports
+// TODO: Convert this to use composition API.
 Vue.mixin({
   computed: {
     DJANGO_API() {
@@ -29,26 +39,24 @@ export default featureToggles;
 
 /** Enables a feature toggle. Only usable from the console. */
 function enable(featureToggle: string) {
-  if (!(featureToggle in featureToggles)) {
-    throw `Feature toggle ${featureToggle} is not defined`;
+  if (!isToggle(featureToggle)) {
+    throw new Error(`Feature toggle ${featureToggle} is not defined`);
   }
-  // @ts-ignore
+
   featureToggles[featureToggle] = true;
   console.log(`Feature toggle '${featureToggle}' enabled`);
 }
 
 /** Disables a feature toggle. Only usable from the console. */
 function disable(featureToggle: string) {
-  if (!(featureToggle in featureToggles)) {
-    throw `Feature toggle ${featureToggle} is not defined`;
+  if (!isToggle(featureToggle)) {
+    throw new Error(`Feature toggle ${featureToggle} is not defined`);
   }
-  // @ts-ignore
+
   featureToggles[featureToggle] = false;
   console.log(`Feature toggle '${featureToggle}' disabled`);
 }
 
 // Register enable and disable on the window object so they are accessible from the console
-// @ts-ignore
 window.enable = enable;
-// @ts-ignore
 window.disable = disable;
---
+++ web/src/plugins/girder.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+
+// @ts-ignore missing definitions
+import Girder from '@girder/components/src';
+
+Vue.use(Girder);
