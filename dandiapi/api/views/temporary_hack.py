36fe0159adc3f74f652a3ead782d88dc677e807b
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-01-25 18:15:39 -0500

    Add endpoints to map ids

---
+++ dandiapi/api/views/temporary_hack.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.http.response import HttpResponseBase
+from django.shortcuts import get_object_or_404
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from dandiapi.api.dandiset_migration import move_if_necessary
+from dandiapi.api.models import Dandiset
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def map_ids_view(request: Request) -> HttpResponseBase:
+    for dandiset in Dandiset.objects.all():
+        move_if_necessary(dandiset)
+    return Response()
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def map_id_view(request: Request, dandiset_id) -> HttpResponseBase:
+    dandiset = get_object_or_404(Dandiset, pk=dandiset_id)
+    move_if_necessary(dandiset)
+    return Response()
