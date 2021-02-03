9e0156276bba16a84c491fe0eadb54cea51c4d6f
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-02-03 14:10:47 -0500

    Add schemaURL field to /info/

--- dandiapi/api/views/info.py
+++ dandiapi/api/views/info.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+schema_url = (
+    'https://raw.githubusercontent.com/dandi/schema/master/'
+    f'releases/{settings.DANDI_SCHEMA_VERSION}/dandiset.json'
+)
+
 
 @api_view()
 def info_view(self):
     return Response(
         {
             'schemaVersion': settings.DANDI_SCHEMA_VERSION,
+            'schemaURL': schema_url,
         }
     )
