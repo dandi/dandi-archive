35a3c4108fbbd64797bf72646308f616f8a36b2d
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-02-23 18:50:26 -0500

    Remove search endpoint

--- dandiapi/api/views/__init__.py
+++ dandiapi/api/views/__init__.py
@@ -1,7 +1,6 @@
 from .asset import AssetViewSet
 from .auth import auth_token_view
 from .dandiset import DandisetViewSet
-from .search import search_view
 from .stats import stats_view
 from .upload import (
     upload_complete_view,
@@ -23,6 +22,5 @@ __all__ = [
     'upload_get_validation_view',
     'users_me_view',
     'users_search_view',
-    'search_view',
     'stats_view',
 ]
