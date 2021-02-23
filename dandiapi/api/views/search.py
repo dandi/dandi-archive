c2ae080d4a0afa73ee2fa8d4a3557ef935072e53
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-02-23 16:34:44 -0500

    Apply SearchVector directly to metadata for search

--- dandiapi/api/views/search.py
+++ dandiapi/api/views/search.py
@@ -1,5 +1,4 @@
-from django.db.models import TextField
-from django.db.models.functions import Cast
+from django.contrib.postgres.search import SearchVector
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
@@ -25,9 +24,8 @@ def search_view(request):
     if 'search' not in request.query_params:
         return Response([])
 
-    # Currently only guaranteed on identifier search
-    versions = Version.objects.annotate(
-        text_metadata=Cast('metadata__metadata', TextField())
-    ).filter(text_metadata__search=request.query_params['search'])
+    versions = Version.objects.annotate(search=SearchVector('metadata__metadata')).filter(
+        search=request.query_params['search']
+    )
 
     return Response(VersionSerializer(versions, many=True).data)
