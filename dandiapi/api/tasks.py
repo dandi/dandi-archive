466c9d29af5da004aa78cb4e3c362fe388bd4fa2
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-03-16 15:57:34 -0400

    Rename variable in calulate_sha256 task

--- dandiapi/api/tasks.py
+++ dandiapi/api/tasks.py
@@ -12,14 +12,14 @@ logger = get_task_logger(__name__)
 @atomic
 def calculate_sha256(uuid: int) -> None:
     logger.info('Starting sha256 calculation for blob %s', uuid)
-    asset = AssetBlob.objects.get(uuid=uuid)
+    asset_blob = AssetBlob.objects.get(uuid=uuid)
 
-    sha256 = calculate_sha256_checksum(asset.blob.storage, asset.blob.name)
+    sha256 = calculate_sha256_checksum(asset_blob.blob.storage, asset_blob.blob.name)
     logger.info('Calculated sha256 %s', sha256)
 
     # TODO: Run dandi-cli validation
 
     logger.info('Saving sha256 %s to blob %s', sha256, uuid)
 
-    asset.sha256 = sha256
-    asset.save()
+    asset_blob.sha256 = sha256
+    asset_blob.save()
