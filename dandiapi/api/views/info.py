fc931209449ccd57d62840c9c4d1bc19e79bf044
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-04-06 18:34:57 -0400

    Add rudimentary version reporting

--- dandiapi/api/views/info.py
+++ dandiapi/api/views/info.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from drf_yasg.utils import no_body, swagger_auto_schema
+import requests
 from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,6 +14,7 @@ schema_url = (
 class ApiInfoSerializer(serializers.Serializer):
     schema_version = serializers.CharField()
     schema_url = serializers.URLField()
+    tag = serializers.CharField()
 
 
 @swagger_auto_schema(
@@ -22,10 +24,17 @@ class ApiInfoSerializer(serializers.Serializer):
 )
 @api_view()
 def info_view(self):
+    # Right now the production deployment is tracking master, which makes tags basically useless.
+    # TODO: Deploy from tags, and report that tag here.
+    resp = requests.get('https://api.github.com/repos/dandi/dandi-api/releases')
+    tags = [release['tag_name'] for release in resp.json()]
+    tag = tags[0]
+
     serializer = ApiInfoSerializer(
         data={
             'schema_version': settings.DANDI_SCHEMA_VERSION,
             'schema_url': schema_url,
+            'tag': tag,
         }
     )
     serializer.is_valid(raise_exception=True)
