cad22f0824f3d862d54d26cdf700378908db4f6e
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-04-07 13:49:08 -0400

    Update the info endpoint to show versioneer info

--- dandiapi/api/views/info.py
+++ dandiapi/api/views/info.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from drf_yasg.utils import no_body, swagger_auto_schema
-import requests
 from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+import versioneer
 
 schema_url = (
     'https://raw.githubusercontent.com/dandi/schema/master/'
@@ -14,7 +15,7 @@ schema_url = (
 class ApiInfoSerializer(serializers.Serializer):
     schema_version = serializers.CharField()
     schema_url = serializers.URLField()
-    tag = serializers.CharField()
+    version = serializers.CharField()
 
 
 @swagger_auto_schema(
@@ -24,17 +25,12 @@ class ApiInfoSerializer(serializers.Serializer):
 )
 @api_view()
 def info_view(self):
-    # Right now the production deployment is tracking master, which makes tags basically useless.
-    # TODO: Deploy from tags, and report that tag here.
-    resp = requests.get('https://api.github.com/repos/dandi/dandi-api/releases')
-    tags = [release['tag_name'] for release in resp.json()]
-    tag = tags[0]
 
     serializer = ApiInfoSerializer(
         data={
             'schema_version': settings.DANDI_SCHEMA_VERSION,
             'schema_url': schema_url,
-            'tag': tag,
+            'version': versioneer.get_version(),
         }
     )
     serializer.is_valid(raise_exception=True)
