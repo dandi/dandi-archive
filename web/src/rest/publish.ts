7324400b73dbd32acd80a77b7dff2e749bb7c523
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-04-14 15:38:31 -0400

    Fix user search URL typo

--- web/src/rest/publish.ts
+++ web/src/rest/publish.ts
@@ -175,7 +175,7 @@ const publishRest = new Vue({
       return client.put(`dandisets/${identifier}/users/`, owners);
     },
     async searchUsers(username: string): Promise<User[]> {
-      const { data } = await client.get('users/search/?', { params: { username } });
+      const { data } = await client.get('users/search/', { params: { username } });
       return data;
     },
     async publish(identifier: string): Promise<Version> {
