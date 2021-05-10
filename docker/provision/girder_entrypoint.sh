0dc820af72ac8be912636b236d33dfab570079da
Brian Helba <brian.helba@kitware.com>
2021-05-10 16:49:22 -0400

    Remove obsolete Dockerfiles
    These support the Girder 3 plugin. This also removes "client.Dockerfile",
    but that's now much simpler to run natively for development.

--- docker/provision/girder_entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-girder serve --database mongodb://mongo:27017/girder --host 0.0.0.0
--- docker/provision/provision_entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sleep 10
-python3 /home/provision/provision_girder.py
-python3 /home/provision/populate_data.py
