1ad492ddd466b33fddd95d3369b6b9442b42a364
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-05-18 09:31:30 -0400

    Fix dependency of migration 0008

--- dandiapi/api/migrations/0008_version_validation_status.py
+++ dandiapi/api/migrations/0008_version_validation_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0006_default_oauth_application'),
+        ('api', '0007_validation_status'),
     ]
 
     operations = [
