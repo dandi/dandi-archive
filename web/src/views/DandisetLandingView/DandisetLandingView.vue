ac1eeab4b1affe8ad4451af359c4c67c27f60585
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-05-25 11:46:06 -0400

    Fix broken access of user when null

--- web/src/views/DandisetLandingView/DandisetLandingView.vue
+++ web/src/views/DandisetLandingView/DandisetLandingView.vue
@@ -162,6 +162,10 @@ export default {
   asyncComputed: {
     userCanModifyDandiset: {
       async get() {
+        if (!this.user) {
+          return false;
+        }
+
         if (this.user.admin) {
           return true;
         }
