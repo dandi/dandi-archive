ca1c43d14510c1621b2c9353c0aa8e820346044c
Roni Choudhury <roni.choudhury@kitware.com>
2021-06-01 16:50:22 -0400

    Increase max page size to 1000

--- dandiapi/api/views/common.py
+++ dandiapi/api/views/common.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class DandiPagination(PageNumberPagination):
     page_size = 25
-    max_page_size = 100
+    max_page_size = 1000
     page_size_query_param = 'page_size'
