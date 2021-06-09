5496471169563233f592a122fda5f6eb36e27f34
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-06-09 10:22:33 -0400

    Add migrate_version_metadata script

---
+++ scripts/migrate_version_metadata.py
@@ -0,0 +1,18 @@
+from dandischema import migrate
+from dandiapi.api.models import Version, VersionMetadata
+
+
+def run(to_version):
+    print(f'Migrating all version metadata to version {to_version}')
+    for version in Version.objects.all():
+        print(f'Migrating {version.dandiset.identifier}/{version.version}')
+        metanew = migrate(version.metadata.metadata, to_version=to_version, skip_validation=True)
+        new: VersionMetadata
+        new, created = VersionMetadata.objects.get_or_create(
+            name=version.metadata.name,
+            metadata=metanew,
+        )
+        if created:
+            new.save()
+        version.metadata = new
+        version.save()
