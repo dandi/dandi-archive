0ec42fc1e1af675fe1fc0a2c758604dc0200e4ed
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2021-06-22 16:45:38 -0400

    Make sort by `modified` default instead of `name` (#756)

--- web/src/components/DandisetsPage.vue
+++ web/src/components/DandisetsPage.vue
@@ -78,16 +78,16 @@ const DANDISETS_PER_PAGE = 8;
 
 const sortingOptions = [
   {
-    name: 'Name',
-    djangoField: 'name',
+    name: 'Modified',
+    djangoField: 'modified',
   },
   {
     name: 'Identifier',
     djangoField: 'id',
   },
   {
-    name: 'Modified',
-    djangoField: 'modified',
+    name: 'Name',
+    djangoField: 'name',
   },
 ];
 
