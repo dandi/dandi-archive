17c0721dc893ed87080a238ca9c95ff0f64d7a69
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-06-28 19:06:49 -0400

    Inject a schemaVersion if not present

--- scripts/migrate_version_metadata.py
+++ scripts/migrate_version_metadata.py
@@ -1,4 +1,5 @@
 from dandischema import migrate
+from django.conf import settings
 
 from dandiapi.api.models import Version, VersionMetadata
 from dandiapi.api.tasks import validate_version_metadata
@@ -8,7 +9,14 @@ def run(to_version):
     print(f'Migrating all version metadata to version {to_version}')
     for version in Version.objects.filter(version='draft'):
         print(f'Migrating {version.dandiset.identifier}/{version.version}')
-        metanew = migrate(version.metadata.metadata, to_version=to_version, skip_validation=True)
+
+        metadata = version.metadata.metadata
+        # If there is no schemaVersion, assume the most recent
+        if 'schemaVersion' not in metadata:
+            metadata['schemaVersion'] = settings.DANDI_SCHEMA_VERSION
+
+        metanew = migrate(metadata, to_version=to_version, skip_validation=True)
+
         new: VersionMetadata
         new, created = VersionMetadata.objects.get_or_create(
             name=version.metadata.name,
