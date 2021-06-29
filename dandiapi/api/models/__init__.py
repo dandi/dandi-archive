01de2055697a1b1fe8cf8f178a0aea550431d9e5
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-06-29 16:58:41 -0400

    rename model, add documentation

--- dandiapi/api/models/__init__.py
+++ dandiapi/api/models/__init__.py
@@ -1,15 +1,15 @@
 from .asset import Asset, AssetBlob, AssetMetadata
 from .dandiset import Dandiset
-from .oauth import Application
+from .oauth import StagingApplication
 from .upload import Upload
 from .version import Version, VersionMetadata
 
 __all__ = [
-    'Application',
     'Asset',
     'AssetBlob',
     'AssetMetadata',
     'Dandiset',
+    'StagingApplication',
     'Upload',
     'Version',
     'VersionMetadata',
--- dandiapi/settings.py
+++ dandiapi/settings.py
@@ -85,6 +85,11 @@ class HerokuProductionConfiguration(DandiMixin, HerokuProductionBaseConfiguratio
     LOGIN_URL = '/accounts/github/login/'
 
 
-# use the same config for staging as production, but also allow regex for OAuth redirect URIs
+# NOTE: The staging configuration uses a custom OAuth toolkit `Application` model
+# (`StagingApplication`) to allow for wildcards in OAuth redirect URIs (to support Netlify branch
+# deploy previews, etc). Note that both the custom `StagingApplication` and default
+# `oauth2_provider.models.Application` will have Django database models and will show up on the
+# Django admin, but only one of them will be in active use depending on the environment
+# the API server is running in (production/local or staging).
 class HerokuStagingConfiguration(HerokuProductionBaseConfiguration):
-    OAUTH2_PROVIDER_APPLICATION_MODEL = 'api.Application'
+    OAUTH2_PROVIDER_APPLICATION_MODEL = 'api.StagingApplication'
