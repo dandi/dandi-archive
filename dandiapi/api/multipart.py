63ef2f21e81ddc9b1809e5c867cdf98ebd78f358
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-07-28 17:22:53 -0400

    Bump dandischema and replace dandi-cli dependency

--- dandiapi/api/multipart.py
+++ dandiapi/api/multipart.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from typing import Iterator, Tuple
 
-from dandi.core.digests.dandietag import PartGenerator
+from dandischema.digests.dandietag import PartGenerator
 from django.core.files.storage import Storage
 from s3_file_field._multipart import MultipartManager
 from s3_file_field._multipart_boto3 import Boto3MultipartManager
