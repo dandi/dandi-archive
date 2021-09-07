b837b72b05b3163ac7834536ecbb23c530bdaade
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-09-07 09:03:37 -0400

    Update dandiapi/api/checks.py
    Co-authored-by: Yaroslav Halchenko <debian@onerussian.com>

--- dandiapi/api/checks.py
+++ dandiapi/api/checks.py
@@ -15,7 +15,7 @@ def check_doi_settings(app_configs, **kwargs):
         if setting is None:
             errors.append(
                 Error(
-                    f'Setting {name} is not specified, but other DOI settings were.',
+                    f'Setting {name} is not specified, but other DOI settings are.',
                     hint=f'Define {name} as an environment variable.',
                     obj=settings,
                 )
