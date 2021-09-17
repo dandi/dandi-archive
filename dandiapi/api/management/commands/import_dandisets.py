2c194142a9e3212463b256408774b65913653c9d
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-09-17 11:20:58 -0400

    Break out single dandiset import logic to another function

--- dandiapi/api/management/commands/import_dandisets.py
+++ dandiapi/api/management/commands/import_dandisets.py
@@ -38,33 +38,37 @@ def import_versions_from_response(api_url: str, version_api_response: dict, dand
 
 
 @transaction.atomic
+def import_dandiset_from_response(api_url: str, dandiset_api_response: dict):
+    # Check if dandiset with this name already exists. If it does, skip it.
+    # I can't think of a better way to uniquely identify dandisets
+    # without being able to use identifiers.
+    existing_dandiset = Version.objects.filter(name=dandiset_api_response['draft_version']['name'])
+    if existing_dandiset.first():
+        identifier = existing_dandiset.first().dandiset.identifier
+        click.echo(
+            f'Skipping {dandiset_api_response["draft_version"]["name"]} - dandiset {identifier} already exists with that name.'  # noqa: E501
+        )
+        return
+
+    identifier = dandiset_api_response['identifier']
+    dandiset = Dandiset()
+    dandiset.save()
+
+    # get all versions associated with this dandiset
+    versions = requests.get(urljoin(api_url, f'/api/dandisets/{identifier}/versions/')).json()
+
+    click.echo(
+        f'Importing dandiset {identifier} "{dandiset_api_response["draft_version"]["name"]}" as dandiset {dandiset.identifier}'  # noqa: E501
+    )
+
+    import_versions_from_response(api_url, versions, dandiset)
+
+
+@transaction.atomic
 def import_dandisets_from_response(api_url: str, dandiset_api_response: dict):
     """Import dandisets given a response from /api/dandisets/."""
     for result in dandiset_api_response['results']:
-
-        # Check if dandiset with this name already exists. If it does, skip it.
-        # I can't think of a better way to uniquely identify dandisets
-        # without being able to use identifiers.
-        existing_dandiset = Version.objects.filter(name=result['draft_version']['name'])
-        if existing_dandiset.first():
-            identifier = existing_dandiset.first().dandiset.identifier
-            click.echo(
-                f'Skipping {result["draft_version"]["name"]} - dandiset {identifier} already exists with that name.'  # noqa: E501
-            )
-            continue
-
-        identifier = result['identifier']
-        dandiset = Dandiset()
-        dandiset.save()
-
-        # get all versions associated with this dandiset
-        versions = requests.get(urljoin(api_url, f'/api/dandisets/{identifier}/versions/')).json()
-
-        click.echo(
-            f'Importing dandiset {identifier} "{result["draft_version"]["name"]}" as dandiset {dandiset.identifier}'  # noqa: E501
-        )
-
-        import_versions_from_response(api_url, versions, dandiset)
+        import_dandiset_from_response(api_url, result)
 
     # Handle API pagination
     if dandiset_api_response.get('next'):
@@ -78,5 +82,4 @@ def import_dandisets(api_url: str):
     click.echo(f'Importing all dandisets from dandi-api deployment at {api_url}')
 
     dandisets = requests.get(urljoin(api_url, '/api/dandisets/')).json()
-
     import_dandisets_from_response(api_url, dandisets)
