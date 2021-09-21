b67716e96d53e40d29eecff18e167c8b4955e6ee
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-09-21 14:50:27 -0400

    Add `offset` option

--- dandiapi/api/management/commands/import_dandisets.py
+++ dandiapi/api/management/commands/import_dandisets.py
@@ -1,10 +1,31 @@
 from urllib.parse import urljoin
 
-from django.db import transaction
+from django.db import connection, transaction
+from django.db.utils import OperationalError
 import djclick as click
 import requests
 
 from dandiapi.api.models import Dandiset, Version
+from dandiapi.api.tasks import validate_version_metadata
+
+
+@transaction.atomic
+def set_sequence_value(new_value: int):
+    """Set api_dandiset_id_seq value."""
+    cursor = connection.cursor()
+    cursor.execute(f'ALTER SEQUENCE api_dandiset_id_seq RESTART WITH {new_value}')
+
+
+@transaction.atomic
+def get_sequence_value():
+    """Get current api_dandiset_id_seq value."""
+    cursor = connection.cursor()
+    try:
+        cursor.execute('SELECT last_value FROM api_dandiset_id_seq')
+    except OperationalError:
+        cursor.execute('CREATE SEQUENCE api_dandiset_id_seq START 1')
+        cursor.execute('SELECT last_value FROM api_dandiset_id_seq')
+    return cursor.fetchone()[0]
 
 
 @transaction.atomic
@@ -39,7 +60,10 @@ def import_versions_from_response(api_url: str, version_api_response: dict, dand
 
 @transaction.atomic
 def import_dandiset_from_response(
-    api_url: str, dandiset_api_response: dict, dandiset_to_replace=None, skip_existing_names=True
+    api_url: str,
+    dandiset_api_response: dict,
+    dandiset_to_replace=None,
+    skip_existing_names=True,
 ):
     if skip_existing_names:
         # Check if dandiset with this name already exists. If it does, skip it.
@@ -80,12 +104,15 @@ def import_dandiset_from_response(
 def import_dandisets_from_response(api_url: str, dandiset_api_response: dict):
     """Import dandisets given a response from /api/dandisets/."""
     for result in dandiset_api_response['results']:
-        import_dandiset_from_response(api_url, result)
+        import_dandiset_from_response(api_url, result, skip_existing_names=False)
 
     # Handle API pagination
     if dandiset_api_response.get('next'):
         new_dandisets = requests.get(dandiset_api_response.get('next')).json()
-        import_dandisets_from_response(api_url, new_dandisets)
+        import_dandisets_from_response(
+            api_url,
+            new_dandisets,
+        )
 
 
 @click.command()
@@ -93,18 +120,27 @@ def import_dandisets_from_response(api_url: str, dandiset_api_response: dict):
 @click.option('--all', is_flag=True, required=False, help='Download all dandisets.')
 @click.option('--identifier', required=False, help='The identifier of the dandiset to import.')
 @click.option('--replace', default=None, help='The dandiset to replace with the imported one.')
-def import_dandisets(api_url: str, all: bool, identifier: str, replace: str):
+@click.option('--offset', default=0, help="Offset to add to each new dandiset's identifier.")
+@transaction.atomic
+def import_dandisets(api_url: str, all: bool, identifier: str, replace: str, offset=0):
+    original_sequence_value = get_sequence_value()
+    set_sequence_value(original_sequence_value + offset)
     if all:
         click.echo(f'Importing all dandisets from dandi-api deployment at {api_url}')
         dandisets = requests.get(urljoin(api_url, '/api/dandisets/')).json()
         import_dandisets_from_response(api_url, dandisets)
+        set_sequence_value(original_sequence_value)
 
     elif identifier:
         click.echo(f'Importing dandiset {identifier} from dandi-api deployment at {api_url}')
         dandiset = requests.get(urljoin(api_url, f'/api/dandisets/{identifier}/')).json()
         import_dandiset_from_response(
-            api_url, dandiset, dandiset_to_replace=replace, skip_existing_names=False
+            api_url,
+            dandiset,
+            dandiset_to_replace=replace,
+            skip_existing_names=False,
         )
+        set_sequence_value(original_sequence_value)
 
     else:
         click.echo(
