eab8c9f391552c1d16cac4e7b257884a28ce52bf
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-09-30 14:11:50 -0400

    Replace scheduled draft manifest job with call in scheduled validation job

--- dandiapi/api/scheduled_tasks.py
+++ dandiapi/api/scheduled_tasks.py
@@ -17,17 +17,6 @@ logger = get_task_logger(__name__)
 
 @shared_task
 @atomic
-def write_draft_manifest_files():
-    logger.info('Writing manifest files for all draft versions')
-    # TODO Optimize this if it causes too much load in production.
-    # Rewriting every draft manifest every time is guaranteed not to miss any changes,
-    # so just do that for now to avoid the complexity involved with modification dates.
-    for draft_version in Version.objects.filter(version='draft').all():
-        write_manifest_files.delay(draft_version.id)
-
-
-@shared_task
-@atomic
 def validate_draft_version_metadata():
     logger.info('Checking for draft versions that need validation')
     # Select only the id of draft versions that have status PENDING
@@ -38,11 +27,12 @@ def validate_draft_version_metadata():
     for draft_version in pending_draft_versions:
         validate_version_metadata.delay(draft_version['id'])
 
+        # Revalidation should be triggered every time a version is modified,
+        # so now is a good time to write out the manifests as well.
+        write_manifest_files.delay(draft_version.id)
+
 
 def register_scheduled_tasks(sender, **kwargs):
     """Register tasks with a celery beat schedule."""
-    # Write out all draft manifests every day at 1 AM
-    sender.add_periodic_task(crontab(hour=1), write_draft_manifest_files.s())
-
     # Check for any draft versions that need validation every 2 minutes
     sender.add_periodic_task(crontab(minute='*/2'), validate_draft_version_metadata.s())
