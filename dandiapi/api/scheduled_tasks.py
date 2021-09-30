cb5e79ea797ba2e7e1a04bdd968e6824cae5e795
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-09-30 14:01:38 -0400

    Add scheduled task for pending validations

--- dandiapi/api/scheduled_tasks.py
+++ dandiapi/api/scheduled_tasks.py
@@ -10,7 +10,7 @@ from celery.utils.log import get_task_logger
 from django.db.transaction import atomic
 
 from dandiapi.api.models import Version
-from dandiapi.api.tasks import write_manifest_files
+from dandiapi.api.tasks import write_manifest_files, validate_version_metadata
 
 logger = get_task_logger(__name__)
 
@@ -26,7 +26,23 @@ def write_draft_manifest_files():
         write_manifest_files.delay(draft_version.id)
 
 
+@shared_task
+@atomic
+def validate_draft_version_metadata():
+    logger.info('Checking for draft versions that need validation')
+    # Select only the id of draft versions that have status PENDING
+    pending_draft_versions = (
+        Version.objects.filter(status=Version.Status.PENDING).filter(version='draft').values('id')
+    )
+    logger.info('Found %s versions to validate', pending_draft_versions.count())
+    for draft_version in pending_draft_versions:
+        validate_version_metadata.delay(draft_version['id'])
+
+
 def register_scheduled_tasks(sender, **kwargs):
     """Register tasks with a celery beat schedule."""
     # Write out all draft manifests every day at 1 AM
     sender.add_periodic_task(crontab(hour=1), write_draft_manifest_files.s())
+
+    # Check for any draft versions that need validation every 2 minutes
+    sender.add_periodic_task(crontab(minute='*/2'), validate_draft_version_metadata.s())
