a6cd026c7fff02d96c70f53b06ea6540719021a1
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-10-19 11:23:38 -0400

    linting

--- dandiapi/api/signals.py
+++ dandiapi/api/signals.py
@@ -1,7 +1,6 @@
 from allauth.account.signals import user_signed_up
 from corsheaders.signals import check_request_enabled
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
