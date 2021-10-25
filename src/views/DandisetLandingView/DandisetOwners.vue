c4b88226662190233f8f555aa1d25dfc2d63df79
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-10-25 13:24:57 -0400

    Restore old ownership ui + modify button

--- src/views/DandisetLandingView/DandisetOwners.vue
+++ src/views/DandisetLandingView/DandisetOwners.vue
@@ -1,181 +1,145 @@
 <template>
   <v-card
-    v-if="currentDandiset"
-    height="100%"
-    class="px-3 py-1"
     outlined
+    class="mt-4 px-3 pb-5"
   >
     <v-row
       no-gutters
-      class="my-1 ml-1"
+      class="my-1"
     >
-      <div class="black--text text-h5">
-        Ownership
-      </div>
-    </v-row>
-    <v-row>
-      <v-tooltip
-        v-if="showOwnerSearchBox"
-        top
-        :disabled="userCanModifyDandiset"
-      >
-        <template #activator="{ on }">
-          <div v-on="on">
-            <v-row class="mx-2 px-2">
-              <v-autocomplete
-                v-model="selection"
-                :items="items"
-                :disabled="!userCanModifyDandiset"
-                :loading="loadingUsers"
-                :search-input.sync="search"
-                hide-no-data
-                clearable
-                auto-select-first
-                item-text="result"
-                placeholder="enter email address"
-                outlined
-                flat
-                return-object
-                @update:search-input="throttledUpdate"
-              />
-            </v-row>
-          </div>
-        </template>
-        <template v-if="loggedIn">
-          You must be an owner to manage ownership.
-        </template>
-        <template v-else>
-          You must be logged in to manage ownership.
-        </template>
-      </v-tooltip>
       <v-row
-        v-if="owners && owners.length"
-        class="mx-2 px-2"
+        no-gutters
+        class="my-1 ml-1"
       >
+        <div class="black--text text-h5">
+          Owners
+        </div>
+      </v-row>
+    </v-row>
+
+    <v-row class="my-1">
+      <v-col cols="12">
         <v-chip
-          v-for="(owner, i) in newOwners"
-          :key="i"
-          color="light-blue lighten-4"
-          text-color="light-blue darken-3"
+          v-for="(owner, i) in limitedOwners"
+          :key="owner.name || owner.username"
+          color="grey lighten-4"
+          text-color="grey darken-2"
           class="font-weight-medium ma-1"
-          :close="userCanModifyDandiset"
-          @click:close="removeOwner(i)"
+          style="border: 1px solid #E0E0E0 !important;"
+          close-icon="mdi-star-circle"
+          :close="i === 0"
         >
           {{ owner.name || owner.username }}
         </v-chip>
-      </v-row>
+        <span
+          v-if="numExtraOwners"
+          class="ml-1 text--secondary"
+        >
+          +{{ numExtraOwners }} more...
+        </span>
+      </v-col>
+    </v-row>
 
-      <v-row
-        v-else
-        class="justify-center"
+    <v-row
+      class="justify-center"
+      no-gutters
+    >
+      <v-dialog
+        v-model="ownerDialog"
+        width="50%"
       >
-        No owners
-      </v-row>
+        <template #activator="{ on }">
+          <v-tooltip
+            :disabled="!manageOwnersDisabled"
+            left
+          >
+            <template #activator="{ on: tooltipOn }">
+              <div
+                style="width: 100%;"
+                v-on="tooltipOn"
+              >
+                <v-btn
+                  id="manage"
+                  depressed
+                  :disabled="manageOwnersDisabled"
+                  color="light-blue lighten-5"
+                  class="light-blue--text text--lighten-1 justify-start"
+                  block
+                  v-on="on"
+                >
+                  <v-icon
+                    class="pr-2"
+                  >
+                    mdi-account-plus
+                  </v-icon>
+                  Manage Owners
+                </v-btn>
+              </div>
+            </template>
+            <template v-if="loggedIn">
+              You must be an owner to manage ownership.
+            </template>
+            <template v-else>
+              You must be logged in to manage ownership.
+            </template>
+          </v-tooltip>
+        </template>
+        <DandisetOwnersDialog
+          :key="ownerDialogKey"
+          :owners="owners"
+          @close="ownerDialog = false"
+        />
+      </v-dialog>
     </v-row>
   </v-card>
 </template>
 
-<script lang="ts">
-import {
-  defineComponent, reactive, ref, computed, Ref, ComputedRef, watch,
-} from '@vue/composition-api';
+<script>
+import { mapState } from 'vuex';
+import { loggedIn, user } from '@/rest';
+import DandisetOwnersDialog from './DandisetOwnersDialog.vue';
 
-import { debounce } from 'lodash';
-
-import { draftVersion } from '@/utils/constants';
-import { dandiRest, loggedIn as loggedInFunc } from '@/rest';
-import store from '@/store';
-import { User } from '@/types';
-
-interface UserResult extends User {
-  result: string
-}
-
-// Includes a field `result` on each user which is the value displayed in the UI
-const appendResult = (users: User[]): UserResult[] => users?.map((u: User) => ({ ...u, result: (u.name) ? `${u.name} (${u.username})` : u.username }));
-
-export default defineComponent({
+export default {
   name: 'DandisetOwners',
-  props: {
-    userCanModifyDandiset: {
-      type: Boolean,
-      required: true,
-    },
+  components: {
+    DandisetOwnersDialog,
   },
-  setup() {
-    const currentDandiset = computed(() => store.state.dandiset.dandiset);
-    const owners = computed(() => store.state.dandiset.owners);
-
-    const search = ref('');
-    const loadingUsers = ref(false);
-    const selection: Ref<UserResult|null> = ref(null);
-    const items: Ref<User[]> = ref([]);
-
-    const newOwners: UserResult[] = reactive([]);
-    watch(() => owners.value, () => (
-      owners.value ? Object.assign(newOwners, appendResult(owners.value)) : null),
-    { immediate: true });
-
-    const loggedIn: ComputedRef<boolean> = computed(loggedInFunc);
-
-    const showOwnerSearchBox = computed(
-      // Only show the search box to logged in users on DRAFT versions
-      () => loggedIn.value && currentDandiset.value?.version === draftVersion,
-    );
-
-    function setOwners(ownersToSet: User[]) {
-      store.commit.dandiset.setOwners(ownersToSet);
-    }
-
-    const throttledUpdate = debounce((async () => {
-      if (!search.value) return;
-
-      loadingUsers.value = true;
-      const users = await dandiRest.searchUsers(search.value);
-      items.value = appendResult(users);
-      loadingUsers.value = false;
-    }), 200);
-
-    async function save() {
-      if (currentDandiset.value?.dandiset) {
-        const { identifier } = currentDandiset.value.dandiset;
-        const { data } = await dandiRest.setOwners(identifier, newOwners);
-        setOwners(data);
-      }
-    }
-
-    async function removeOwner(index: number) {
-      newOwners.splice(index, 1);
-      await save();
-    }
-
-    watch(selection, async () => {
-      // Verify that the selected user hasn't already been selected
-      if (selection.value && !newOwners.find((x) => x.username === selection.value?.username)) {
-        newOwners.push(selection.value);
-      }
-      // Clear the search field, if it isn't already
-      if (selection.value) {
-        selection.value = null;
-      }
-
-      await save();
-    });
-
+  data() {
     return {
-      currentDandiset,
-      owners,
-      selection,
-      items,
-      loadingUsers,
-      search,
-      throttledUpdate,
-      loggedIn,
-      newOwners,
-      removeOwner,
-      draftVersion,
-      showOwnerSearchBox,
+      labelClasses: 'mx-2 text--secondary',
+      itemClasses: 'font-weight-medium',
+      ownerDialog: false,
+      ownerDialogKey: 0,
     };
   },
-});
+  computed: {
+    user,
+    loggedIn,
+    currentDandiset() {
+      return this.currentDandiset;
+    },
+    manageOwnersDisabled() {
+      if (!this.loggedIn || !this.owners) return true;
+      return !this.owners.find((owner) => owner.username === this.user.username);
+    },
+    limitedOwners() {
+      if (!this.owners) return [];
+      return this.owners.slice(0, 5);
+    },
+    numExtraOwners() {
+      if (!this.owners) return 0;
+      return this.owners.length - this.limitedOwners.length;
+    },
+    ...mapState('dandiset', {
+      currentDandiset: (state) => state.dandiset,
+      owners: (state) => state.owners,
+    }),
+  },
+  watch: {
+    ownerDialog() {
+      // This is incremented to force re-render of the owner dialog
+      this.ownerDialogKey += 1;
+    },
+  },
+};
 </script>
