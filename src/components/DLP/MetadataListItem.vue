205c184d9de58d31a4166ad85c80e28e3d01283d
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-10-27 11:13:58 -0400

    Reuseable component for DLP metadata listings

---
+++ src/components/DLP/MetadataListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    :class="backgroundColor"
+    style="width: 100%;"
+  >
+    <div
+      class="pl-2 my-1 py-1"
+      :style="`border-left: medium solid ${borderLeftColor};
+               line-height: 1.25`"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'ListItem',
+  props: {
+    backgroundColor: {
+      type: String,
+      default: 'white',
+    },
+  },
+  setup(props, ctx) {
+    const { $vuetify } = ctx.root;
+
+    const borderLeftColor = computed(() => $vuetify.theme.themes.light.primary);
+
+    return { borderLeftColor };
+  },
+});
+</script>
