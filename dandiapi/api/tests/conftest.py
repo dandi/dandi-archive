f6d20d1a4167f6cbc00d9665f599676408703d08
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-10-29 15:59:26 -0400

    Update existing asset tests to include zarr

--- dandiapi/api/tests/conftest.py
+++ dandiapi/api/tests/conftest.py
@@ -10,8 +10,6 @@ from pytest_factoryboy import register
 from rest_framework.test import APIClient
 from storages.backends.s3boto3 import S3Boto3Storage
 
-from dandiapi.api.models import UserMetadata
-
 from .factories import (
     AssetBlobFactory,
     DandisetFactory,
@@ -68,7 +66,6 @@ def api_client() -> APIClient:
 
 @pytest.fixture
 def authenticated_api_client(user) -> APIClient:
-    UserMetadata.objects.create(user=user)
     client = APIClient()
     client.force_authenticate(user=user)
     return client
