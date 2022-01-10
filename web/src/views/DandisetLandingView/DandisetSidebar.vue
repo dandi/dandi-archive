9ca2442b5b908019e8fde83586006ec596fdb884
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2022-01-10 17:55:33 -0500

    Disable embargo dlp ui

--- web/src/views/DandisetLandingView/DandisetSidebar.vue
+++ web/src/views/DandisetLandingView/DandisetSidebar.vue
@@ -5,7 +5,7 @@
       :user-can-modify-dandiset="userCanModifyDandiset"
     />
 
-    <div v-if="currentDandiset.dandiset.embargo_status === 'OPEN'">
+    <div v-if="true">
       <DandisetPublish
         :user-can-modify-dandiset="userCanModifyDandiset"
       />
