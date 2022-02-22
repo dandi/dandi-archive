9f73f25e5fe468b3969a0fdf173a40156ea0546c
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-02-21 21:38:17 -0500

    Combine tests

--- dandiapi/api/tests/test_compute_zarr_checksum.py
+++ dandiapi/api/tests/test_compute_zarr_checksum.py
@@ -13,35 +13,36 @@ from dandiapi.api.zarr_checksums import (
 
 
 @pytest.mark.django_db
-def test_compute_zarr_checksum(zarr_upload_file_factory, zarr_archive_factory):
+def test_compute_zarr_checksum(zarr_upload_file_factory, zarr_archive_factory, faker):
     zarr: ZarrArchive = zarr_archive_factory()
 
-    # Add initial files
-    a = zarr_upload_file_factory(zarr_archive=zarr, path='foo/a')
-    b = zarr_upload_file_factory(zarr_archive=zarr, path='foo/b')
-    c = zarr_upload_file_factory(zarr_archive=zarr, path='foo/bar/c')
+    # Generate > 1000 files, since the page size from S3 is 1000 items
+    foo_bar_files: List[ZarrUploadFile] = [
+        zarr_upload_file_factory(zarr_archive=zarr, path='foo/bar/a'),
+        zarr_upload_file_factory(zarr_archive=zarr, path='foo/bar/b'),
+    ]
+    foo_baz_files: List[ZarrUploadFile] = [
+        zarr_upload_file_factory(zarr_archive=zarr, path=f'foo/baz/{faker.pystr()}')
+        for _ in range(1005)
+    ]
 
+    # Generate correct listings
     serializer = ZarrJSONChecksumSerializer()
-    foo_bar_listing = serializer.generate_listing(files=[c.to_checksum()])
+    foo_bar_listing = serializer.generate_listing(files=[f.to_checksum() for f in foo_bar_files])
+    foo_baz_listing = serializer.generate_listing(files=[f.to_checksum() for f in foo_baz_files])
     foo_listing = serializer.generate_listing(
-        files=[
-            a.to_checksum(),
-            b.to_checksum(),
-        ],
         directories=[
             ZarrChecksum(path='foo/bar', md5=foo_bar_listing.md5),
-        ],
-    )
-
-    # The root contains an entry for foo
-    root_listing = serializer.generate_listing(
-        directories=[
-            ZarrChecksum(path='foo', md5=foo_listing.md5),
+            ZarrChecksum(path='foo/baz', md5=foo_baz_listing.md5),
         ]
+    )
+    root_listing = serializer.generate_listing(
+        directories=[ZarrChecksum(path='foo', md5=foo_listing.md5)]
     )
 
     # Assert checksum files don't already exist
     assert ZarrChecksumFileUpdater(zarr, 'foo/bar').read_checksum_file() is None
+    assert ZarrChecksumFileUpdater(zarr, 'foo/baz').read_checksum_file() is None
     assert ZarrChecksumFileUpdater(zarr, 'foo').read_checksum_file() is None
     assert ZarrChecksumFileUpdater(zarr, '').read_checksum_file() is None
 
@@ -50,6 +51,7 @@ def test_compute_zarr_checksum(zarr_upload_file_factory, zarr_archive_factory):
 
     # Assert files computed correctly
     assert ZarrChecksumFileUpdater(zarr, 'foo/bar').read_checksum_file() == foo_bar_listing
+    assert ZarrChecksumFileUpdater(zarr, 'foo/baz').read_checksum_file() == foo_baz_listing
     assert ZarrChecksumFileUpdater(zarr, 'foo').read_checksum_file() == foo_listing
     assert ZarrChecksumFileUpdater(zarr, '').read_checksum_file() == root_listing
 
@@ -102,44 +104,5 @@ def test_compute_zarr_checksum_existing(zarr_upload_file_factory, zarr_archive_f
 
     # Assert files computed correctly
     assert ZarrChecksumFileUpdater(zarr, 'foo/bar').read_checksum_file() == foo_bar_listing
-    assert ZarrChecksumFileUpdater(zarr, 'foo').read_checksum_file() == foo_listing
-    assert ZarrChecksumFileUpdater(zarr, '').read_checksum_file() == root_listing
-
-
-@pytest.mark.django_db
-def test_compute_zarr_checksum_many_files(zarr_upload_file_factory, zarr_archive_factory, faker):
-    zarr: ZarrArchive = zarr_archive_factory()
-
-    # Generate > 1000 files, since the page size from S3 is 1000 items
-    foo_bar_files: List[ZarrUploadFile] = [
-        zarr_upload_file_factory(zarr_archive=zarr, path='foo/bar/a'),
-        zarr_upload_file_factory(zarr_archive=zarr, path='foo/bar/b'),
-    ]
-    foo_baz_files: List[ZarrUploadFile] = [
-        # zarr_upload_file_factory(zarr_archive=zarr, path=f'foo/baz/{faker.file_name()}')
-        zarr_upload_file_factory(zarr_archive=zarr, path=f'foo/baz/{faker.pystr()}')
-        for _ in range(1005)
-    ]
-
-    # Generate correct listings
-    serializer = ZarrJSONChecksumSerializer()
-    foo_bar_listing = serializer.generate_listing(files=[f.to_checksum() for f in foo_bar_files])
-    foo_baz_listing = serializer.generate_listing(files=[f.to_checksum() for f in foo_baz_files])
-    foo_listing = serializer.generate_listing(
-        directories=[
-            ZarrChecksum(path='foo/bar', md5=foo_bar_listing.md5),
-            ZarrChecksum(path='foo/baz', md5=foo_baz_listing.md5),
-        ]
-    )
-    root_listing = serializer.generate_listing(
-        directories=[ZarrChecksum(path='foo', md5=foo_listing.md5)]
-    )
-
-    # Compute checksum
-    compute_zarr_checksum(str(zarr.id))
-
-    # Assert files computed correctly
-    assert ZarrChecksumFileUpdater(zarr, 'foo/bar').read_checksum_file() == foo_bar_listing
-    assert ZarrChecksumFileUpdater(zarr, 'foo/baz').read_checksum_file() == foo_baz_listing
     assert ZarrChecksumFileUpdater(zarr, 'foo').read_checksum_file() == foo_listing
     assert ZarrChecksumFileUpdater(zarr, '').read_checksum_file() == root_listing
