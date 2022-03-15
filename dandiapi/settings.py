9c8ccdbba95107124bd508bdef463fc3623e2591
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-03-15 14:19:05 -0400

    Add comment explaining `STATICFILES_STORAGE` setting

--- dandiapi/settings.py
+++ dandiapi/settings.py
@@ -25,6 +25,8 @@ class DandiMixin(ConfigMixin):
 
     DANDI_ALLOW_LOCALHOST_URLS = False
 
+    # Workaround for static file storage to work correctly on Django 4.
+    # Taken from https://github.com/axnsan12/drf-yasg/issues/761#issuecomment-1031381674
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
     @staticmethod
