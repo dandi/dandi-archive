c29edf998dee85f0099bfe3de6977b3b9919ebab
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-04-11 18:15:09 -0400

    [WIP] Add extra fields to /api/info endpoint

--- dandiapi/api/views/info.py
+++ dandiapi/api/views/info.py
@@ -12,10 +12,28 @@ schema_url = (
 )
 
 
+class ApiServiceSerializer(serializers.Serializer):
+    url = serializers.CharField()
+
+
+class ApiServicesSerializer(serializers.Serializer):
+    api = ApiServiceSerializer()
+    webui = ApiServiceSerializer()
+    jupyterhub = ApiServiceSerializer()
+
+
 class ApiInfoSerializer(serializers.Serializer):
+    # Schema
     schema_version = serializers.CharField()
     schema_url = serializers.URLField()
+
+    # Versions
     version = serializers.CharField()
+    cli_minimal_version = serializers.CharField()
+    cli_bad_versions = serializers.ListField(child=serializers.CharField())
+
+    # Services
+    services = ApiServicesSerializer()
 
 
 @swagger_auto_schema(
@@ -25,12 +43,21 @@ class ApiInfoSerializer(serializers.Serializer):
 )
 @api_view()
 def info_view(self):
-
     serializer = ApiInfoSerializer(
         data={
             'schema_version': settings.DANDI_SCHEMA_VERSION,
             'schema_url': schema_url,
+            # TODO: Use git describe --abbrev=0
             'version': versioneer.get_version(),
+            # TODO: Determine what these should be
+            "cli_minimal_version": "0.14.2",
+            "cli_bad_versions": [],
+            "services": {
+                "api": {"url": settings.DANDI_API_URL},
+                "webui": {"url": settings.DANDI_WEB_APP_URL},
+                # TODO: Add jupyter hub api setting
+                "jupyterhub": {"url": "https://hub.dandiarchive.org"},
+            },
         }
     )
     serializer.is_valid(raise_exception=True)
