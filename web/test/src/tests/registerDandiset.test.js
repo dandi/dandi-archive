e782d122c0178636b64dba0ee36618668a615cb6
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-07-11 13:39:52 -0400

    Give vuetify dropdown 500ms to render in tests

--- web/test/src/tests/registerDandiset.test.js
+++ web/test/src/tests/registerDandiset.test.js
@@ -19,6 +19,7 @@ describe('dandiset registration page', () => {
     await expect(page).toFillXPath(vTextField('Name*'), name);
     await expect(page).toFillXPath(vTextarea('Description*'), description);
     await expect(page).toClickXPath('//label[contains(.,"License*")]/following::input[1]');
+    await page.waitForTimeout(500); // Give dropdown time to render
     await expect(page).toClickXPath(vListItem('spdx:CC0-1.0'));
     await page.waitForTimeout(500); // Form validation can *sometimes* take too long
 
