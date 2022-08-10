001eb4c151fb91df25c96cc90994d34bf071112e
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-08-10 15:31:38 -0400

    Update zarr checksum migration tests

--- dandiapi/api/tests/test_migrate_zarr_checksums.py
+++ dandiapi/api/tests/test_migrate_zarr_checksums.py
@@ -1,4 +1,3 @@
-from guardian.shortcuts import assign_perm
 import pytest
 
 from dandiapi.api.management.commands.migrate_zarr_checksums import migrate_zarr_checksums
@@ -7,21 +6,8 @@ from dandiapi.api.tasks.zarr import ingest_zarr_archive
 
 
 @pytest.mark.django_db
-def test_migrate_zarr_checksums_empty(user, zarr_archive: ZarrArchive):
-    assign_perm('owner', user, zarr_archive.dandiset)
-
-    # Assert checksum is None beforehand
-    assert zarr_archive.checksum is None
-    migrate_zarr_checksums()
-
-    # Assert values have changed
-    zarr_archive.refresh_from_db()
-    assert zarr_archive.checksum is not None
-
-
-@pytest.mark.django_db
-def test_migrate_zarr_checksums_existing(user, zarr_archive: ZarrArchive, zarr_upload_file_factory):
-    assign_perm('owner', user, zarr_archive.dandiset)
+def test_migrate_zarr_checksums(zarr_archive_factory, zarr_upload_file_factory):
+    zarr_archive: ZarrArchive = zarr_archive_factory()
 
     # Create files
     for _ in range(10):
@@ -45,3 +31,29 @@ def test_migrate_zarr_checksums_existing(user, zarr_archive: ZarrArchive, zarr_u
     zarr_archive.refresh_from_db()
     assert zarr_archive.checksum is not None
     assert zarr_archive.status == ZarrArchive.Status.COMPLETE
+
+
+@pytest.mark.django_db
+def test_migrate_zarr_checksums_uningested(zarr_archive_factory, zarr_upload_file_factory):
+    zarr_archive: ZarrArchive = zarr_archive_factory()
+
+    # Create files
+    for _ in range(10):
+        zarr_upload_file_factory(zarr_archive=zarr_archive)
+
+    # We need to complete the upload before the checksum files to be written.
+    zarr_archive.complete_upload()
+
+    # Intentionally don't run ingestion
+    # Intentionally update checksum and status fields, to simulate the migration
+    zarr_archive.checksum = None
+    zarr_archive.status = ZarrArchive.Status.PENDING
+    zarr_archive.save()
+
+    # Migrate checksums
+    migrate_zarr_checksums()
+
+    # Assert values have changed
+    zarr_archive.refresh_from_db()
+    assert zarr_archive.checksum is None
+    assert zarr_archive.status == ZarrArchive.Status.PENDING
