0447586cc81f05c52b42633278d4e732c23c5990
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-08-10 14:57:48 -0400

    Don't dispatch ingest as part of checksum migrate

--- dandiapi/api/management/commands/migrate_zarr_checksums.py
+++ dandiapi/api/management/commands/migrate_zarr_checksums.py
@@ -2,7 +2,6 @@ from django.db.models.query import QuerySet
 import djclick as click
 
 from dandiapi.api.models.zarr import ZarrArchive
-from dandiapi.api.tasks.zarr import ingest_zarr_archive
 
 
 @click.command()
@@ -15,12 +14,8 @@ def migrate_zarr_checksums(*args, **kwargs):
         # Attempt to fetch checksumm from S3
         checksum = zarr.get_checksum()
 
-        # If checksum exists, just save
+        # If checksum exists, save
         if checksum is not None:
             zarr.checksum = checksum
             zarr.status = ZarrArchive.Status.COMPLETE
             zarr.save(update_fields=['status', 'checksum'])
-
-        # Otherwise, dispatch zarr ingestion task
-        else:
-            ingest_zarr_archive.delay(zarr.zarr_id, force=True)
