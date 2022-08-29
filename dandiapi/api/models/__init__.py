e97ae0c9c74e4169b84d62be2134eb46e3d52a9e
Dan LaManna <dan.lamanna@kitware.com>
2022-08-29 14:07:33 -0400

    Create zarr app and move models

--- dandiapi/api/models/__init__.py
+++ dandiapi/api/models/__init__.py
@@ -4,7 +4,6 @@ from .oauth import StagingApplication
 from .upload import EmbargoedUpload, Upload
 from .user import UserMetadata
 from .version import Version
-from .zarr import EmbargoedZarrArchive, EmbargoedZarrUploadFile, ZarrArchive, ZarrUploadFile
 
 __all__ = [
     'Asset',
@@ -12,12 +11,8 @@ __all__ = [
     'Dandiset',
     'EmbargoedAssetBlob',
     'EmbargoedUpload',
-    'EmbargoedZarrArchive',
-    'EmbargoedZarrUploadFile',
     'StagingApplication',
     'Upload',
     'UserMetadata',
     'Version',
-    'ZarrArchive',
-    'ZarrUploadFile',
 ]
