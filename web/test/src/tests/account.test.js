5178d13655362aadd844d852324a6f4196c53da6
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-10-04 13:36:46 -0400

    Add additional waits to account E2E test

--- web/test/src/tests/account.test.js
+++ web/test/src/tests/account.test.js
@@ -9,6 +9,7 @@ import {
   LOGIN_BUTTON_TEXT,
   LOGOUT_BUTTON_TEXT,
   TEST_USER_INITIALS,
+  waitForRequestsToFinish,
 } from '../util';
 
 describe('account management', () => {
@@ -16,8 +17,9 @@ describe('account management', () => {
     await registerNewUser();
 
     await expect(page).toClickXPath(vAvatar(TEST_USER_INITIALS));
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(500); // wait for vuetify animation to finish
     await expect(page).toClickXPath(vListItem(LOGOUT_BUTTON_TEXT, { action: vIcon('mdi-logout') }));
+    await waitForRequestsToFinish();
 
     // this text is only displayed when not logged in
     await expect(page).toMatch(LOGIN_BUTTON_TEXT);
@@ -28,10 +30,13 @@ describe('account management', () => {
 
     // Logout
     await expect(page).toClickXPath(vAvatar(TEST_USER_INITIALS));
+    await page.waitForTimeout(500); // wait for vuetify animation to finish
     await expect(page).toClickXPath(vListItem(LOGOUT_BUTTON_TEXT, { action: vIcon('mdi-logout') }));
+    await waitForRequestsToFinish();
 
     // Test logging in
     await expect(page).toClickXPath(vBtn(LOGIN_BUTTON_TEXT));
+    await waitForRequestsToFinish();
 
     // the user avatar contains the initials and is only rendered when logged in successfully
     await expect(page).toContainXPath(vAvatar(TEST_USER_INITIALS));
