4f6849beb6d5f5eebb6af21f8a2d110bd0f37e17
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-10-17 12:51:25 -0400

    Update asset path management command

--- dandiapi/api/management/commands/ingest_asset_paths.py
+++ dandiapi/api/management/commands/ingest_asset_paths.py
@@ -1,17 +1,12 @@
 import djclick as click
 
-from dandiapi.api.asset_paths import add_asset_paths
+from dandiapi.api.asset_paths import add_version_asset_paths
 from dandiapi.api.models import Version
-
-
-def ingest_version_assets(version: Version):
-    print(f'\t {version.assets.count()} assets')
-    for asset in version.assets.all().iterator():
-        add_asset_paths(asset, version)
 
 
 @click.command()
 def ingest_asset_paths():
-    for version in Version.objects.all().iterator():
+    for version in Version.objects.iterator():
         print(f'Version: {version}')
-        ingest_version_assets(version)
+        print(f'\t {version.assets.count()} assets')
+        add_version_asset_paths(version)
