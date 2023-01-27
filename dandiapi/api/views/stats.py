b20a40a3c7fd815878d6089a5191e5f8aa1977cd
Jacob Nesbitt <jjnesbitt2@gmail.com>
2023-01-27 12:38:03 -0500

    Add 12 hour cache to stats view

--- dandiapi/api/views/stats.py
+++ dandiapi/api/views/stats.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
+from django.views.decorators.cache import cache_page
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from dandiapi.api.models import Asset, Dandiset
 
 
+# Cache this response for 12 hours
+@cache_page(60 * 60 * 12)
 @api_view()
 def stats_view(self):
     dandiset_count = Dandiset.objects.count()
