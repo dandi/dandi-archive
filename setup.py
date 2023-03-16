516119d48d680c51bd4b092672d8f76a5b6f5342
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2023-03-15 22:07:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'celery',
-        'dandischema~=0.8.1',
+        'dandischema~=0.8.2',
         'django~=4.1.0',
         'django-admin-display',
         'django-allauth',
