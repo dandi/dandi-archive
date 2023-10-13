753b5d1487049e75934331adbbfc35a262d9be2f
Yaroslav Halchenko <debian@onerussian.com>
2023-10-13 11:52:25 -0400

    [DATALAD RUNCMD] Apply formatting using black etc
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "tox -e format -- scripts/produce_md_table_of_files.py",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- scripts/produce_md_table_of_files.py
+++ scripts/produce_md_table_of_files.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 import argparse
-import subprocess
 from operator import itemgetter
+import subprocess
+
 
 def git_first_commit_date(filename):
     date_str = subprocess.getoutput(f"git log --follow --format=%ai -- {filename} | tail -n 1")
     return date_str.split()[0]  # Return only the date portion, ignoring time and timezone
+
 
 def git_describe_contains(commit_hash):
     result = subprocess.run(
         ["git", "describe", "--contains", commit_hash],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        text=True
+        text=True,
     )
     return result.stdout.strip() if result.returncode == 0 else ""
+
 
 def git_first_commit_hash(filename):
     return subprocess.getoutput(f"git log --pretty=format:%H -- {filename} | tail -n 1")
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Generate a Markdown table for files in a Git repo.")
+    parser = argparse.ArgumentParser(
+        description="Generate a Markdown table for files in a Git repo."
+    )
     parser.add_argument("--header", type=str, help="Header for the Markdown file.")
     parser.add_argument("-o", "--output", type=str, help="Output Markdown file name.")
     parser.add_argument("files", nargs="+", help="Files to include in the table.")
@@ -30,30 +36,34 @@ def main():
     output_lines = []
     if args.header:
         output_lines.extend(args.header.split(r'\n'))
-    
-    headers = ["File", "Originating Date", "Version", "Implementation State", "Date", "Version", "Superseded by"]
+
+    headers = [
+        "File",
+        "Originating Date",
+        "Version",
+        "Implementation State",
+        "Date",
+        "Version",
+        "Superseded by",
+    ]
     col_widths = [len(h) for h in headers]
 
     data = []
-    
+
     for filename in args.files:
         date = git_first_commit_date(filename)
         commit_hash = git_first_commit_hash(filename)
         version = git_describe_contains(commit_hash)
-        
+
         col_widths[0] = max(col_widths[0], len(filename))
         col_widths[1] = max(col_widths[1], len(date))
         col_widths[2] = max(col_widths[2], len(version))
 
-        data.append({
-            "filename": filename,
-            "date": date,
-            "version": version
-        })
+        data.append({"filename": filename, "date": date, "version": version})
 
     col_widths[-3] = col_widths[1]
     col_widths[-2] = col_widths[2]
-    
+
     sorted_data = sorted(data, key=itemgetter('date'))
 
     header_line = "| " + " | ".join([h.ljust(w) for h, w in zip(headers, col_widths)]) + " |"
@@ -75,6 +85,7 @@ def main():
             f.write("\n".join(output_lines) + "\n")
     else:
         print("\n".join(output_lines))
+
 
 if __name__ == "__main__":
     main()
