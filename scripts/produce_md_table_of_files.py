7276269a35ac053bdaf7ff4e45cded9e077fe520
Yaroslav Halchenko <debian@onerussian.com>
2023-10-13 11:53:31 -0400

    [DATALAD RUNCMD] Replace all " with ' to please flake8
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "sed -e 's,\",'\"'\"',g' -i scripts/produce_md_table_of_files.py",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- scripts/produce_md_table_of_files.py
+++ scripts/produce_md_table_of_files.py
@@ -6,31 +6,31 @@ import subprocess
 
 
 def git_first_commit_date(filename):
-    date_str = subprocess.getoutput(f"git log --follow --format=%ai -- {filename} | tail -n 1")
+    date_str = subprocess.getoutput(f'git log --follow --format=%ai -- {filename} | tail -n 1')
     return date_str.split()[0]  # Return only the date portion, ignoring time and timezone
 
 
 def git_describe_contains(commit_hash):
     result = subprocess.run(
-        ["git", "describe", "--contains", commit_hash],
+        ['git', 'describe', '--contains', commit_hash],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
     )
-    return result.stdout.strip() if result.returncode == 0 else ""
+    return result.stdout.strip() if result.returncode == 0 else ''
 
 
 def git_first_commit_hash(filename):
-    return subprocess.getoutput(f"git log --pretty=format:%H -- {filename} | tail -n 1")
+    return subprocess.getoutput(f'git log --pretty=format:%H -- {filename} | tail -n 1')
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Generate a Markdown table for files in a Git repo."
+        description='Generate a Markdown table for files in a Git repo.'
     )
-    parser.add_argument("--header", type=str, help="Header for the Markdown file.")
-    parser.add_argument("-o", "--output", type=str, help="Output Markdown file name.")
-    parser.add_argument("files", nargs="+", help="Files to include in the table.")
+    parser.add_argument('--header', type=str, help='Header for the Markdown file.')
+    parser.add_argument('-o', '--output', type=str, help='Output Markdown file name.')
+    parser.add_argument('files', nargs='+', help='Files to include in the table.')
     args = parser.parse_args()
 
     output_lines = []
@@ -38,13 +38,13 @@ def main():
         output_lines.extend(args.header.split(r'\n'))
 
     headers = [
-        "File",
-        "Originating Date",
-        "Version",
-        "Implementation State",
-        "Date",
-        "Version",
-        "Superseded by",
+        'File',
+        'Originating Date',
+        'Version',
+        'Implementation State',
+        'Date',
+        'Version',
+        'Superseded by',
     ]
     col_widths = [len(h) for h in headers]
 
@@ -59,33 +59,33 @@ def main():
         col_widths[1] = max(col_widths[1], len(date))
         col_widths[2] = max(col_widths[2], len(version))
 
-        data.append({"filename": filename, "date": date, "version": version})
+        data.append({'filename': filename, 'date': date, 'version': version})
 
     col_widths[-3] = col_widths[1]
     col_widths[-2] = col_widths[2]
 
     sorted_data = sorted(data, key=itemgetter('date'))
 
-    header_line = "| " + " | ".join([h.ljust(w) for h, w in zip(headers, col_widths)]) + " |"
-    separator_line = "| " + " | ".join(["-" * w for w in col_widths]) + " |"
+    header_line = '| ' + ' | '.join([h.ljust(w) for h, w in zip(headers, col_widths)]) + ' |'
+    separator_line = '| ' + ' | '.join(['-' * w for w in col_widths]) + ' |'
 
     output_lines.append(header_line)
     output_lines.append(separator_line)
 
     for entry in sorted_data:
-        filename = entry["filename"].ljust(col_widths[0])
-        date = entry["date"].ljust(col_widths[1])
-        version = entry["version"].ljust(col_widths[2])
-        empty_cols = [" " * w for w in col_widths[3:]]
-        row_line = "| " + " | ".join([filename, date, version] + empty_cols) + " |"
+        filename = entry['filename'].ljust(col_widths[0])
+        date = entry['date'].ljust(col_widths[1])
+        version = entry['version'].ljust(col_widths[2])
+        empty_cols = [' ' * w for w in col_widths[3:]]
+        row_line = '| ' + ' | '.join([filename, date, version] + empty_cols) + ' |'
         output_lines.append(row_line)
 
     if args.output:
-        with open(args.output, "w") as f:
-            f.write("\n".join(output_lines) + "\n")
+        with open(args.output, 'w') as f:
+            f.write('\n'.join(output_lines) + '\n')
     else:
-        print("\n".join(output_lines))
+        print('\n'.join(output_lines))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
