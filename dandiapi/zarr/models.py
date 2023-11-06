74d630bc817039cb24e7e13820a39a2951ebe78e
Brian Helba <brian.helba@kitware.com>
2023-11-06 16:09:28 -0500

    Fix RUF010 Use explicit conversion flag

--- dandiapi/zarr/models.py
+++ dandiapi/zarr/models.py
@@ -102,7 +102,7 @@ class ZarrArchive(BaseZarrArchive):
 
     def s3_path(self, zarr_path: str | Path):
         """Generate a full S3 object path from a path in this zarr_archive."""
-        return f'{settings.DANDI_DANDISETS_BUCKET_PREFIX}{settings.DANDI_ZARR_PREFIX_NAME}/{self.zarr_id}/{str(zarr_path)}'  # noqa: E501
+        return f'{settings.DANDI_DANDISETS_BUCKET_PREFIX}{settings.DANDI_ZARR_PREFIX_NAME}/{self.zarr_id}/{zarr_path}'  # noqa: E501
 
 
 class EmbargoedZarrArchive(BaseZarrArchive):
@@ -113,4 +113,4 @@ class EmbargoedZarrArchive(BaseZarrArchive):
 
     def s3_path(self, zarr_path: str | Path):
         """Generate a full S3 object path from a path in this zarr_archive."""
-        return f'{settings.DANDI_DANDISETS_EMBARGO_BUCKET_PREFIX}{settings.DANDI_ZARR_PREFIX_NAME}/{self.dandiset.identifier}/{self.zarr_id}/{str(zarr_path)}'  # noqa: E501
+        return f'{settings.DANDI_DANDISETS_EMBARGO_BUCKET_PREFIX}{settings.DANDI_ZARR_PREFIX_NAME}/{self.dandiset.identifier}/{self.zarr_id}/{zarr_path}'  # noqa: E501
