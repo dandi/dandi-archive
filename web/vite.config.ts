1009a5d9d439a2f203263c1abe62ea61773ea3a7
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-11-07 14:15:30 -0500

    Add git revision to vite config
    Taken from vue.config.js

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,4 @@
+import { execSync } from 'node:child_process'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -6,6 +7,29 @@ import vue2 from '@vitejs/plugin-vue2'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
+
+function getVersion() {
+  // Try to get the version via `git` if available; otherwise fall back on
+  // the COMMIT_REF environment variable provided by Netlify's build
+  // environment; if that is missing, report "unknown" as the version.
+
+  try {
+    return execSync('git describe --tags').toString();
+  } catch (err) {
+    return process.env.COMMIT_REF ? process.env.COMMIT_REF : 'unknown';
+  }
+}
+
+function getGitRevision() {
+  try {
+    return execSync('git rev-parse HEAD').toString();
+  } catch (err) {
+    return '';
+  }
+}
+
+process.env.VITE_APP_VERSION = getVersion();
+process.env.VITE_APP_GIT_REVISION = getGitRevision();
 
 
 // https://vitejs.dev/config/
