77c79338ea72fca980f77fb1ea6201a4a34690ae
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2023-11-19 18:24:23 -0500

    more exact

--- dandiapi/api/migrations/0006_default_oauth_application.py
+++ dandiapi/api/migrations/0006_default_oauth_application.py
@@ -5,14 +5,15 @@ from django.db.models import Q
 def create_application(apps, schema_editor):
     Application = apps.get_model('oauth2_provider', 'Application')
     name = 'DANDI GUI'
+    # TODO: Aaron -- This needs to match the value that is passed for the app
     # This is specified so it matches the default value used in the frontend.
     client_id = 'Dk0zosgt1GAAKfN8LT4STJmLJXwMDPbYWYzfNtAl'
     if not Application.objects.filter(Q(name=name) | Q(client_id=client_id)).exists():
         application = Application(
-            # Production instances should change this.
+            # Production instances should change this. Aaron
             client_id='Dk0zosgt1GAAKfN8LT4STJmLJXwMDPbYWYzfNtAl',
-            # Production instances must change this.
-            redirect_uris='http://localhost:8085/',
+            # Production instances must change this. Aaron
+            redirect_uris='https://lincbrain.org',
             # These values should not be modified.
             client_type='public',
             authorization_grant_type='authorization-code',
