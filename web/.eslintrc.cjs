efb8276a403688953aa365267337d911e8d451cb
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-12-07 11:37:11 -0500

    Add linting rules from old config

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -13,6 +13,31 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript",
   ],
+  rules: {
+    camelcase: 'off',
+    'vuetify/no-deprecated-classes': 'error',
+    'vuetify/grid-unknown-attributes': 'error',
+    'vuetify/no-legacy-grid': 'error',
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_id',
+          '_modelType',
+          '_accessLevel',
+        ],
+      },
+    ],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
   settings: {
     "import/resolver": {
       typescript: {
