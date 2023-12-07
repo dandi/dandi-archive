74be41664c4f07df03df0163d7ef73750d03105f
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-12-07 13:54:03 -0500

    Copy vite.config.ts logic from old app

--- vue3/vite.config.ts
+++ vue3/vite.config.ts
@@ -1,8 +1,33 @@
+import { execSync } from 'node:child_process'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify';
+
+function getVersion() {
+  // Try to get the version via `git` if available; otherwise fall back on
+  // the COMMIT_REF environment variable provided by Netlify's build
+  // environment; if that is missing, report "unknown" as the version.
+
+  try {
+    return execSync('git describe --tags').toString();
+  } catch (err) {
+    return process.env.COMMIT_REF ? process.env.COMMIT_REF : 'unknown';
+  }
+}
+
+function getGitRevision() {
+  try {
+    return execSync('git rev-parse HEAD').toString();
+  } catch (err) {
+    return '';
+  }
+}
+
+process.env.VITE_APP_VERSION = getVersion();
+process.env.VITE_APP_GIT_REVISION = getGitRevision();
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
