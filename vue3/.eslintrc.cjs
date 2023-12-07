1d2e7dedc65b05a7fbd0607fa800411ffa5aab18
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-12-07 13:13:42 -0500

    Move config files to vue 3 project

--- vue3/.eslintrc.cjs
+++ vue3/.eslintrc.cjs
@@ -1,14 +1,49 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'plugin:vuetify/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+  rules: {
+    camelcase: 'off',
+    'vuetify/no-deprecated-classes': 'error',
+    'vuetify/grid-unknown-attributes': 'error',
+    'vuetify/no-legacy-grid': 'error',
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_id',
+          '_modelType',
+          '_accessLevel',
+        ],
+      },
+    ],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: '.'
+      }
+    },
+  },
 }
