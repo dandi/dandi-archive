e3fa3e4a60a0ecc080a27bc36067a941eabfe856
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-12-07 14:03:43 -0500

    Add vuetify config to `main.ts`

--- vue3/src/main.ts
+++ vue3/src/main.ts
@@ -4,7 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App)
 
