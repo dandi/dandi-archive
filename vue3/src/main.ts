eea7f6ad26902822a98a2ef0e6ee9f2e0dc2c0cb
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-12-07 20:30:57 -0500

    Move plugins into their own module, include Vuetify theme

--- vue3/src/main.ts
+++ vue3/src/main.ts
@@ -1,22 +1,16 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+// plugins
+import router from '@/router'
+import pinia from '@/plugins/pinia'
+import vuetify from '@/plugins/vuetify'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
