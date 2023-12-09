900c48c2c813fb00d388dbd62a56b59cb4618dab
Brian Helba <brian.helba@kitware.com>
2023-12-08 21:28:56 -0500

    Ensure that creating the default OAuth2 Application works on staging

--- dandiapi/api/migrations/0003_default_oauth_application.py
+++ dandiapi/api/migrations/0003_default_oauth_application.py
@@ -1,16 +1,19 @@
 from django.db import migrations
 from django.db.models import Q
+from oauth2_provider.settings import oauth2_settings
 
 
 def create_application(apps, schema_editor):
-    Application = apps.get_model('oauth2_provider', 'Application')
+    # This could be either "oauth2_provider.Application" or "api.StagingApplication".
+    Application = apps.get_model(oauth2_settings.APPLICATION_MODEL)
+
     name = 'DANDI GUI'
     # This is specified so it matches the default value used in the frontend.
     client_id = 'Dk0zosgt1GAAKfN8LT4STJmLJXwMDPbYWYzfNtAl'
     if not Application.objects.filter(Q(name=name) | Q(client_id=client_id)).exists():
         application = Application(
             # Production instances should change this.
-            client_id='Dk0zosgt1GAAKfN8LT4STJmLJXwMDPbYWYzfNtAl',
+            client_id=client_id,
             # Production instances must change this.
             redirect_uris='http://localhost:8085/',
             # These values should not be modified.
@@ -32,8 +35,11 @@ def reverse_create_application(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('api', '0002_asset_zarr'),
+        # Both Application and StagingApplication should exist, though only one will be used.
+        # Trying to make this actually swappable doesn't work, since "oauth2_provider.Application"
+        # always gets declared when "AbstractApplication" is imported.
         ('oauth2_provider', '0003_auto_20201211_1314'),
+        ('api', '0001_stagingapplication'),
     ]
 
     operations = [migrations.RunPython(create_application, reverse_create_application)]
---
+++ dandiapi/api/migrations/0004_merge.py
@@ -0,0 +1,8 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('api', '0002_asset_zarr'),
+        ('api', '0003_default_oauth_application'),
+    ]
