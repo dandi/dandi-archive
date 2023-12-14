6a7745b3a38c3a1020e9eda8777736a6ae3b6153
Mary Salvi <mary.salvi@kitware.com>
2023-12-14 16:18:41 -0500

    Add an e2e test for the FileBrowser

---
+++ e2e/src/tests/fileBrowserPage.test.js
@@ -0,0 +1,21 @@
+import { vBtn, vListItemSubtitle  } from 'jest-puppeteer-vuetify';
+import {
+    waitForRequestsToFinish,
+  } from '../util';
+
+describe('file browser page', () => {
+    it('view file browser', async () => {
+
+        await expect(page).toClickXPath(vBtn('Public Dandisets'));
+        await waitForRequestsToFinish();
+
+        // Assert that the page has loaded with a list of dandisets
+        await expect(page).toClickXPath(vListItemSubtitle('DANDI:000001'));
+        await expect(page).toClickXPath(vBtn('Files'));
+        await waitForRequestsToFinish();
+
+        // Ensure that the page has loaded with a folder
+        await expect(page).toMatch('foo');
+
+    });
+});
