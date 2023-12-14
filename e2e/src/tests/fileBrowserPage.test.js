641d56716fc1bfdb9ac77be67178d97fcb254a50
Mary Salvi <mary.salvi@kitware.com>
2023-12-14 16:33:58 -0500

    Lint Fixes

--- e2e/src/tests/fileBrowserPage.test.js
+++ e2e/src/tests/fileBrowserPage.test.js
@@ -1,21 +1,19 @@
-import { vBtn, vListItemSubtitle  } from 'jest-puppeteer-vuetify';
+import { vBtn, vListItemSubtitle } from 'jest-puppeteer-vuetify';
 import {
-    waitForRequestsToFinish,
-  } from '../util';
+  waitForRequestsToFinish,
+} from '../util';
 
 describe('file browser page', () => {
-    it('view file browser', async () => {
+  it('view file browser', async () => {
+    await expect(page).toClickXPath(vBtn('Public Dandisets'));
+    await waitForRequestsToFinish();
 
-        await expect(page).toClickXPath(vBtn('Public Dandisets'));
-        await waitForRequestsToFinish();
+    // Assert that the page has loaded with a list of dandisets
+    await expect(page).toClickXPath(vListItemSubtitle('DANDI:000001'));
+    await expect(page).toClickXPath(vBtn('Files'));
+    await waitForRequestsToFinish();
 
-        // Assert that the page has loaded with a list of dandisets
-        await expect(page).toClickXPath(vListItemSubtitle('DANDI:000001'));
-        await expect(page).toClickXPath(vBtn('Files'));
-        await waitForRequestsToFinish();
-
-        // Ensure that the page has loaded with a folder
-        await expect(page).toMatch('foo');
-
-    });
+    // Ensure that the page has loaded with a folder
+    await expect(page).toMatch('foo');
+  });
 });
