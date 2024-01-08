d377a0938e2dc0432f40000cd201a6c9cfbf3a7c
Mary Salvi <mary.salvi@kitware.com>
2024-01-08 13:38:59 -0500

    Address PR comments

--- e2e/src/tests/fileBrowserPage.test.js
+++ e2e/src/tests/fileBrowserPage.test.js
@@ -1,19 +1,18 @@
-import { vBtn, vListItemSubtitle, vList } from 'jest-puppeteer-vuetify';
+import { vBtn, vList } from 'jest-puppeteer-vuetify';
 import {
   waitForRequestsToFinish,
+  CLIENT_URL,
 } from '../util';
 
 describe('file browser page', () => {
   it('view file browser', async () => {
-    await expect(page).toClickXPath(vBtn('Public Dandisets'));
+    await page.goto(new URL('/dandiset/000001', CLIENT_URL).href);
     await waitForRequestsToFinish();
 
-    // Assert that the page has loaded with a list of dandisets
-    await expect(page).toClickXPath(vListItemSubtitle('DANDI:000001'));
     await expect(page).toClickXPath(vBtn('Files'));
     await waitForRequestsToFinish();
 
     // Ensure that the page has loaded with a folder
-    await expect(page).toContainXPath(vList());
+    await expect(page).toContainXPath(vList('foo'));
   });
 });
