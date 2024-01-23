7eda5b86927a8173387c58767920ee748ec9fd11
Yaroslav Halchenko <debian@onerussian.com>
2024-01-23 10:18:25 -0500

    Boost dandischema to 0.9.* series so we get support for pydantic 2.0 and schema 0.6.5

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'celery',
-        'dandischema~=0.8.4',
+        'dandischema~=0.9.0',
         'django~=4.1.0',
         'django-admin-display',
         # Require 0.58.0 as it is the first version to support postgres' native
