a602ae9e6246058da18f89ad6dd2fab9544990b2
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-06-03 19:12:15 -0400

    bump unit test

--- dandiapi/api/tests/test_stats.py
+++ dandiapi/api/tests/test_stats.py
@@ -38,9 +38,10 @@ def test_stats_user(api_client, user):
     # Create users with different statuses
     approved_user_count = 0
     for status in UserMetadata.Status.choices:
-        user = User.objects.create(username=f'{status.lower()}_user')
-        UserMetadata.objects.create(user=user, status=status)
-        if status == UserMetadata.Status.APPROVED:
+        status_value = status[0]
+        user = User.objects.create(username=f'{status_value.lower()}_user')
+        UserMetadata.objects.create(user=user, status=status_value)
+        if status_value == UserMetadata.Status.APPROVED:
             approved_user_count += 1
 
     stats = api_client.get('/api/stats/').data
@@ -61,7 +62,7 @@ def test_stats_asset(api_client, version, asset):
 @pytest.mark.django_db()
 def test_stats_embargoed_asset(api_client, version, asset_factory, embargoed_asset_blob_factory):
     embargoed_asset = asset_factory()
-    embargoed_asset.embargoed_blob = embargoed_asset_blob_factory()
+    embargoed_asset.blob = embargoed_asset_blob_factory()
     version.assets.add(embargoed_asset)
 
     stats = api_client.get('/api/stats/').data
@@ -77,7 +78,7 @@ def test_stats_embargoed_and_regular_blobs(
     version.assets.add(asset)
 
     embargoed_asset = asset_factory()
-    embargoed_asset.embargoed_blob = embargoed_asset_blob_factory()
+    embargoed_asset.blob = embargoed_asset_blob_factory()
     version.assets.add(embargoed_asset)
 
     stats = api_client.get('/api/stats/').data
