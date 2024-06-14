bb0c841881afc804e77a988d1af02569d7d3e7ca
Jacob Nesbitt <jjnesbitt2@gmail.com>
2024-06-14 13:45:25 -0400

    Add DandisetUnEmbargoInProgressError

--- dandiapi/api/services/dandiset/exceptions.py
+++ dandiapi/api/services/dandiset/exceptions.py
@@ -14,3 +14,8 @@ class UnauthorizedEmbargoAccessError(DandiError):
     message = (
         'Authentication credentials must be provided when attempting to access embargoed dandisets'
     )
+
+
+class DandisetUnEmbargoInProgressError(DandiError):
+    http_status_code = status.HTTP_400_BAD_REQUEST
+    message = 'Dandiset modification not allowed during un-embargo'
