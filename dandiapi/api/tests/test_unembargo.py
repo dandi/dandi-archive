10e81b9130855b31ecb7cc8e4bfd090f535866b4
Jacob Nesbitt <jjnesbitt2@gmail.com>
2024-07-01 16:52:33 -0400

    Add unembargo tests

--- dandiapi/api/tests/test_unembargo.py
+++ dandiapi/api/tests/test_unembargo.py
@@ -1,11 +1,23 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+import dandischema
 from guardian.shortcuts import assign_perm
 import pytest
 
 from dandiapi.api.models.dandiset import Dandiset
-from dandiapi.api.services.embargo import AssetBlobEmbargoedError, remove_asset_blob_embargoed_tag
-from dandiapi.api.tasks.scheduled import send_dandisets_to_unembargo_email
+from dandiapi.api.services.embargo import (
+    AssetBlobEmbargoedError,
+    remove_asset_blob_embargoed_tag,
+    unembargo_dandiset,
+)
+from dandiapi.api.services.embargo.exceptions import DandisetActiveUploadsError
+from dandiapi.api.services.exceptions import DandiError
+
+if TYPE_CHECKING:
+    from dandiapi.api.models.asset import AssetBlob
+    from dandiapi.api.models.version import Version
 
 
 @pytest.mark.django_db()
@@ -18,39 +30,131 @@ def test_remove_asset_blob_embargoed_tag_fails_on_embargod(embargoed_asset_blob,
 
 
 @pytest.mark.django_db()
-def test_unembargo_dandiset_sends_emails(
-    api_client, user, dandiset, draft_version_factory, mailoutbox
+def test_kickoff_dandiset_unembargo_dandiset_not_embargoed(
+    api_client, user, dandiset_factory, draft_version_factory
 ):
+    dandiset = dandiset_factory(embargo_status=Dandiset.EmbargoStatus.OPEN)
     draft_version_factory(dandiset=dandiset)
-
-    assign_perm('owner', user, dandiset)
-    api_client.force_authenticate(user=user)
-
-    dandiset.embargo_status = Dandiset.EmbargoStatus.EMBARGOED
-    dandiset.save()
-
-    resp = api_client.post(f'/api/dandisets/{dandiset.identifier}/unembargo/')
-    assert resp.status_code == 200
-
-    # Simulate the scheduled task calling this function
-    send_dandisets_to_unembargo_email()
-
-    assert mailoutbox
-    assert 'unembargo' in mailoutbox[0].subject
-    assert dandiset.identifier in mailoutbox[0].message().get_payload()
-    assert user.username in mailoutbox[0].message().get_payload()
-
-
-@pytest.mark.django_db()
-def test_unembargo_dandiset_lingering_uploads(
-    api_client, user, dandiset_factory, draft_version_factory, upload_factory
-):
-    dandiset = dandiset_factory(embargo_status=Dandiset.EmbargoStatus.EMBARGOED)
-    draft_version_factory(dandiset=dandiset)
-    upload_factory(dandiset=dandiset)
-
     assign_perm('owner', user, dandiset)
     api_client.force_authenticate(user=user)
 
     resp = api_client.post(f'/api/dandisets/{dandiset.identifier}/unembargo/')
     assert resp.status_code == 400
+
+
+@pytest.mark.django_db()
+def test_kickoff_dandiset_unembargo_not_owner(
+    api_client, user, dandiset_factory, draft_version_factory
+):
+    dandiset = dandiset_factory(embargo_status=Dandiset.EmbargoStatus.EMBARGOED)
+    draft_version_factory(dandiset=dandiset)
+    api_client.force_authenticate(user=user)
+
+    resp = api_client.post(f'/api/dandisets/{dandiset.identifier}/unembargo/')
+    assert resp.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_kickoff_dandiset_unembargo_active_uploads(
+    api_client, user, dandiset_factory, draft_version_factory, upload_factory
+):
+    dandiset = dandiset_factory(embargo_status=Dandiset.EmbargoStatus.EMBARGOED)
+    draft_version_factory(dandiset=dandiset)
+    assign_perm('owner', user, dandiset)
+    api_client.force_authenticate(user=user)
+
+    # Test that active uploads prevent unembargp
+    upload_factory(dandiset=dandiset)
+    resp = api_client.post(f'/api/dandisets/{dandiset.identifier}/unembargo/')
+    assert resp.status_code == 400
+
+
+@pytest.mark.django_db()
+def test_kickoff_dandiset_unembargo(api_client, user, draft_version_factory, mailoutbox):
+    draft_version = draft_version_factory(dandiset__embargo_status=Dandiset.EmbargoStatus.EMBARGOED)
+    ds: Dandiset = draft_version.dandiset
+
+    assign_perm('owner', user, ds)
+    api_client.force_authenticate(user=user)
+
+    resp = api_client.post(f'/api/dandisets/{ds.identifier}/unembargo/')
+    assert resp.status_code == 200
+
+    ds.refresh_from_db()
+    assert ds.embargo_status == Dandiset.EmbargoStatus.UNEMBARGOING
+    assert mailoutbox
+    assert 'unembargo' in mailoutbox[0].subject
+    assert ds.identifier in mailoutbox[0].message().get_payload()
+    assert user.username in mailoutbox[0].message().get_payload()
+
+
+@pytest.mark.django_db()
+def test_unembargo_dandiset_not_unembargoing(draft_version_factory):
+    draft_version = draft_version_factory(dandiset__embargo_status=Dandiset.EmbargoStatus.EMBARGOED)
+    ds: Dandiset = draft_version.dandiset
+
+    with pytest.raises(DandiError):
+        unembargo_dandiset(ds)
+
+
+@pytest.mark.django_db()
+def test_unembargo_dandiset_uploads_exist(draft_version_factory, upload_factory):
+    draft_version = draft_version_factory(
+        dandiset__embargo_status=Dandiset.EmbargoStatus.UNEMBARGOING
+    )
+    ds: Dandiset = draft_version.dandiset
+
+    upload_factory(dandiset=ds)
+    with pytest.raises(DandisetActiveUploadsError):
+        unembargo_dandiset(ds)
+
+
+@pytest.mark.django_db()
+def test_unembargo_dandiset(
+    draft_version_factory,
+    asset_factory,
+    embargoed_asset_blob_factory,
+    mocker,
+    mailoutbox,
+    user_factory,
+):
+    draft_version: Version = draft_version_factory(
+        dandiset__embargo_status=Dandiset.EmbargoStatus.UNEMBARGOING
+    )
+    ds: Dandiset = draft_version.dandiset
+    owners = [user_factory() for _ in range(5)]
+    for user in owners:
+        assign_perm('owner', user, ds)
+
+    embargoed_blob: AssetBlob = embargoed_asset_blob_factory()
+    asset = asset_factory(blob=embargoed_blob)
+    draft_version.assets.add(asset)
+    assert embargoed_blob.embargoed
+
+    # Patch this function to check if it's been called, since we can't test the tagging directly
+    patched = mocker.patch('dandiapi.api.services.embargo._delete_asset_blob_tags')
+
+    unembargo_dandiset(ds)
+    patched.assert_called_once()
+
+    embargoed_blob.refresh_from_db()
+    ds.refresh_from_db()
+    draft_version.refresh_from_db()
+    assert not embargoed_blob.embargoed
+    assert ds.embargo_status == Dandiset.EmbargoStatus.OPEN
+    assert (
+        draft_version.metadata['access'][0]['status']
+        == dandischema.models.AccessType.OpenAccess.value
+    )
+
+    # Check that a correct email exists
+    assert mailoutbox
+    assert 'has been unembargoed' in mailoutbox[0].subject
+    payload = mailoutbox[0].message().get_payload()[0].get_payload()
+    assert ds.identifier in payload
+    assert 'has been unembargoed' in payload
+
+    # Check that the email was sent to all owners
+    owner_email_set = {user.email for user in owners}
+    mailoutbox_to_email_set = set(mailoutbox[0].to)
+    assert owner_email_set == mailoutbox_to_email_set
