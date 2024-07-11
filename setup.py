524c835a82ffea9c23a90b128f04c42f9580480e
Jacob Nesbitt <jjnesbitt2@gmail.com>
2024-07-11 15:42:57 -0400

    Pin django-storages to 1.14.3

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
         # Production-only
         'django-composed-configuration[prod]>=0.23.0',
         'django-s3-file-field[s3]>=1.0.0',
-        'django-storages[s3]>=1.14.3',
+        'django-storages[s3]==1.14.3',
         'gunicorn',
         # Development-only, but required
         'django-minio-storage',
