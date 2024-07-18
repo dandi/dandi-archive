388adb0fa7a9c4211939140383f1ce8f786e43e3
Roni Choudhury <2903332+waxlamp@users.noreply.github.com>
2024-07-18 15:44:29 -0400

    Use trailing slash on route
    Co-authored-by: Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>

--- dandiapi/urls.py
+++ dandiapi/urls.py
@@ -106,7 +106,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', DashboardView.as_view(), name='dashboard-index'),
     path('dashboard/user/<str:username>/', user_approval_view, name='user-approval'),
-    path('dashboard/mailchimp', mailchimp_csv_view, name='mailchimp-csv'),
+    path('dashboard/mailchimp/', mailchimp_csv_view, name='mailchimp-csv'),
     # this url overrides the authorize url in oauth2_provider.urls to
     # support our user signup workflow
     re_path(r'^oauth/authorize/$', authorize_view, name='authorize'),
