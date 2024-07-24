cbff89003be0744bf0309812840ff367370b175b
Roni Choudhury <roni.choudhury@kitware.com>
2024-07-24 15:20:01 -0400

    Reformat long line

--- dandiapi/api/views/version.py
+++ dandiapi/api/views/version.py
@@ -121,7 +121,9 @@ class VersionViewSet(NestedViewSetMixin, DetailSerializerMixin, ReadOnlyModelVie
                 locked_version.save()
 
                 audit_record = AuditRecord.update_metadata(
-                    dandiset=locked_version.dandiset, user=request.user, metadata=locked_version.metadata
+                    dandiset=locked_version.dandiset,
+                    user=request.user,
+                    metadata=locked_version.metadata,
                 )
                 audit_record.save()
 
