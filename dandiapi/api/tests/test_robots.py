9dcf7313002b903431bd3995838f9b834d7393b2
Jacob Nesbitt <jjnesbitt2@gmail.com>
2024-11-25 13:43:29 -0500

    Remove whitespace from robots.txt page

--- dandiapi/api/tests/test_robots.py
+++ dandiapi/api/tests/test_robots.py
@@ -1,13 +1,13 @@
-import pytest
+from __future__ import annotations
+
 from django.urls import reverse
+import pytest
+
 
 @pytest.mark.django_db
 def test_robots_txt(api_client):
     response = api_client.get(reverse('robots_txt'))
     assert response.status_code == 200
     assert response['Content-Type'] == 'text/plain'
-    expected_content = """
-    User-agent: *
-    Disallow: /
-    """
+    expected_content = 'User-agent: *\nDisallow: /'
     assert response.content.decode('utf-8').strip() == expected_content.strip()
--- dandiapi/api/views/robots.py
+++ dandiapi/api/views/robots.py
@@ -1,10 +1,8 @@
-from django.views.decorators.cache import cache_page
+from __future__ import annotations
+
 from django.http import HttpResponse
 
-@cache_page(60 * 60 * 24)  # 24 hour cache
+
 def robots_txt_view(request):
-    content = """
-    User-agent: *
-    Disallow: /
-    """
-    return HttpResponse(content, content_type="text/plain")
+    content = 'User-agent: *\nDisallow: /'
+    return HttpResponse(content, content_type='text/plain')
