ab6be5c2b5551ca09f03fce49374f323e1fa494a
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-01-27 23:05:53 -0500

    Run `eslint-plugin-vuetify` on more components

--- vue3/src/components/DandiFooter.vue
+++ vue3/src/components/DandiFooter.vue
@@ -20,22 +20,22 @@
             target="_blank"
             rel="noopener"
             href="https://docs.dandiarchive.org/about/terms/"
-          >Terms</a> 
-          <v-icon x-small>
+          >Terms</a>
+          <v-icon size="x-small">
             mdi-open-in-new
           </v-icon> / <a
             target="_blank"
             rel="noopener"
             href="https://docs.dandiarchive.org/about/policies/"
           >Policies</a>
-          <v-icon x-small>
+          <v-icon size="x-small">
             mdi-open-in-new
           </v-icon> / <a
             target="_blank"
             rel="noopener"
             href="https://github.com/dandi/dandi-archive/blob/master/CODE_OF_CONDUCT.md"
           >Code of Conduct</a>
-          <v-icon x-small>
+          <v-icon size="x-small">
             mdi-open-in-new
           </v-icon>
           <br>
@@ -54,14 +54,14 @@
             rel="noopener"
             href="https://braininitiative.nih.gov/"
           >BRAIN Initiative</a>
-          <v-icon x-small>
+          <v-icon size="x-small">
             mdi-open-in-new
           </v-icon> / <a
             target="_blank"
             rel="noopener"
             href="https://www.nimh.nih.gov/index.shtml"
           >NIMH</a>
-          <v-icon x-small>
+          <v-icon size="x-small">
             mdi-open-in-new
           </v-icon>
           <br>
@@ -70,7 +70,7 @@
             rel="noopener"
             href="https://registry.opendata.aws/dandiarchive/"
           >AWS Open Dataset</a>
-          <v-icon x-small>
+          <v-icon size="x-small">
             mdi-open-in-new
           </v-icon>
           <br>
@@ -79,7 +79,7 @@
             rel="noopener"
             href="https://netlify.com"
           >This site is powered by Netlify</a>
-          <v-icon x-small>
+          <v-icon size="x-small">
             mdi-open-in-new
           </v-icon>
         </v-col>
