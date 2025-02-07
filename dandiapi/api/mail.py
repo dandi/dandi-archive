b05fd2745e260c62dbf3040b70b174477973fc75
Lauren Diaz <lauren.diaz@jhuapl.edu>
2025-02-07 13:26:10 -0500

    change email to admin instead of info

--- dandiapi/api/mail.py
+++ dandiapi/api/mail.py
@@ -24,7 +24,7 @@ BASE_RENDER_CONTEXT = {
 }
 
 # TODO: turn this into a Django setting
-ADMIN_EMAIL = 'info@api-dandi.emberarchive.org'
+ADMIN_EMAIL = 'admin@api-dandi.emberarchive.org'
 
 
 def user_greeting_name(user: User, socialaccount: SocialAccount = None) -> str:
