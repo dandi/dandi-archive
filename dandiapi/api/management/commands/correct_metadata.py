9bc14a9d978a2da351e6d47419ae89400cde63ef
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-03-03 12:41:49 -0500

    Simplify correct_metadata and helper functions
    - Don't allow correct function configuration
    - Remove verbose error handling logic
    - Write manifest files synchronously
    - Only support correction of the `Affiliation` schema key
    - Remove `find_objs` tests against generalized schema key
    - Use transactions to couple logic of metadata save and manifest file generation

--- dandiapi/api/management/commands/correct_metadata.py
+++ dandiapi/api/management/commands/correct_metadata.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 
 from copy import deepcopy
-from typing import TYPE_CHECKING, Any
-
-if TYPE_CHECKING:
-    from collections.abc import Callable
+import sys
+import typing
+from typing import Any
 
 from django.db import transaction
 import djclick as click
 
+from dandiapi.api.manifests import write_dandiset_jsonld, write_dandiset_yaml
 from dandiapi.api.models import Version
-from dandiapi.api.tasks import write_manifest_files
 
 
 @click.command(
@@ -33,7 +32,7 @@ from dandiapi.api.tasks import write_manifest_files
     is_flag=True,
     help="Don't perform any changes, just check for corrupted metadata.",
 )
-def correct_metadata(
+def correct_metadata(  # noqa: C901
     *, dandiset: str | None, dandiset_version: str | None, apply_to_all: bool, check: bool
 ):
     if apply_to_all:
@@ -46,51 +45,46 @@ def correct_metadata(
             'Either `--all` or two arguments (dandiset, dandiset_version) must be provided.'
         )
 
-    # Func to use to correct the metadata (update as needed)
-    correct_func: Callable[[dict], dict | None] = correct_affiliation_corruption
+    # Get version queryset
+    vers = Version.objects.all()
+    if not apply_to_all:
+        dandiset = typing.cast(str, dandiset)
+        vers = vers.filter(dandiset=int(dandiset), version=dandiset_version)
 
-    vers = (
-        Version.objects.all()
-        if apply_to_all
-        else (Version.objects.get(dandiset=dandiset, version=dandiset_version),)
-    )
+    if not vers.exists():
+        click.echo('No matching versions found')
+        return
 
-    # List of changes to be applied to the database represented as tuples of the form
-    # version to be changed and the new metadata
-    changes: list[tuple[Version, dict]] = []
+    # For each version, find and fix metadata corruptions, along with saving out manifest files
     for ver in vers:
-        try:
-            meta_corrected = correct_func(ver.metadata)
-        except Exception as e:
-            click.echo(
-                f'{ver.dandiset.identifier}/{ver.version}: Failed to correct metadata with '
-                f'`{correct_func.__name__}()`'
-            )
-            click.echo(e)
-            raise click.Abort from e
+        new_meta = correct_affiliation_corruption(ver.metadata)
+        if new_meta is None:
+            continue
 
-        if meta_corrected is not None:
-            changes.append((ver, meta_corrected))
+        click.echo(f'Found corruption in {ver}')
+        if check:
+            continue
 
-    if changes:
-        # Apply the changes to the database atomically
+        # Save each version in a separate transaction to avoid de-sync with dandiset yaml/jsonld
         with transaction.atomic():
-            for ver, meta_corrected in changes:
-                ver.metadata = meta_corrected
-                ver.save()  # TODO: should I use `save()` in this case? It has some custom logic.
+            write_dandiset_yaml(ver)
+            write_dandiset_jsonld(ver)
+            click.echo(f'\tWrote dandiset yaml and json for version {ver}')
 
-        for ver, _ in changes:
-            write_manifest_files.delay(ver.id)  # TODO: Please check if this is needed
+            ver.metadata = new_meta
+            ver.save()
 
-            click.echo(
-                f'{ver.dandiset.identifier}/{ver.version}: Metadata corrected with '
-                f'`{correct_func.__name__}()`'
-            )
-    else:
+    # Remaining check is not needed since no data was modified
+    if check:
+        return
+
+    # If we find any un-fixed instances, raise exception
+    remaining = [ver for ver in vers if correct_affiliation_corruption(ver.metadata) is not None]
+    if remaining:
         click.echo(
-            f'No changes. No metadata of the Dandiset version(s) need to be corrected per '
-            f'`{correct_func.__name__}()`'
+            click.style(f'\nFound remaining corrupted versions: {remaining}', fg='red', bold=True)
         )
+        sys.exit(1)
 
 
 def correct_affiliation_corruption(meta: dict) -> dict | None:
@@ -104,42 +98,22 @@ def correct_affiliation_corruption(meta: dict) -> dict | None:
     Note: This function corrects the corruptions described in
         https://github.com/dandi/dandi-schema/issues/276
     """
-    unwanted_fields = ['contactPoint', 'includeInCitation', 'roleName']
+    new_meta = deepcopy(meta)
+    correct_objs(new_meta)
 
-    meta_corrected = deepcopy(meta)
-    affiliation_objs = find_objs(meta_corrected, 'Affiliation')
-
-    corrected = False
-    for obj in affiliation_objs:
-        for field in unwanted_fields:
-            if field in obj:
-                del obj[field]
-                corrected = True
-
-    return meta_corrected if corrected else None
+    return new_meta if new_meta != meta else None
 
 
-def find_objs(instance: Any, schema_key: str) -> list[dict]:
-    """
-    Find JSON objects with a specified `"schemaKey"` field within a data instance.
-
-    :param instance: The data instance to find JSON objects from
-    :param schema_key: The `"schemaKey"` field value
-    :return: The list of JSON objects with the specified `"schemaKey"` in the data instance
-    """
-
-    def find_objs_(data: Any) -> None:
-        if isinstance(data, dict):
-            if 'schemaKey' in data and data['schemaKey'] == schema_key:
-                objs.append(data)
-            for value in data.values():
-                find_objs_(value)
-        elif isinstance(data, list):
-            for item in data:
-                find_objs_(item)
-        else:
-            return
-
-    objs: list[dict] = []
-    find_objs_(instance)
-    return objs
+def correct_objs(data: Any) -> None:
+    if isinstance(data, dict):
+        if 'schemaKey' in data and data['schemaKey'] == 'Affiliation':
+            data.pop('contactPoint', None)
+            data.pop('includeInCitation', None)
+            data.pop('roleName', None)
+        for value in data.values():
+            correct_objs(value)
+    elif isinstance(data, list):
+        for item in data:
+            correct_objs(item)
+    else:
+        return
