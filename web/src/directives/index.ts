7b08ab2365740eb109e8630886b207b6ca0d294e
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-03-05 14:52:59 -0500

    Update v-page-title directive for Vue 3

---
+++ web/src/directives/index.ts
@@ -0,0 +1,30 @@
+import type { App, DirectiveBinding } from 'vue'
+
+const TITLE = 'DANDI Archive';
+
+// Function to set the page title based on the directive's binding value
+const setPageTitle = (el: HTMLElement, binding: DirectiveBinding) => {
+  console.log(binding)
+  if (binding?.value) {
+    document.title = `${binding.value} - ${TITLE}`;
+  } else {
+    document.title = TITLE;
+  }
+};
+
+const directives = {
+  'page-title': {
+    // Use mounted and updated lifecycle hooks instead of inserted and unbind
+    mounted: setPageTitle,
+    updated: setPageTitle,
+    beforeUnmount: () => {
+      document.title = TITLE;
+    },
+  }
+}
+
+export function registerDirectives(app: App) {
+  Object.entries(directives).forEach(([name, directive]) => {
+    app.directive(name, directive);
+  });
+}
