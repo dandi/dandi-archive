fc4aa6948b8065cf5f8c9b970426e91b3ac9720e
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-03-13 11:42:14 -0400

    Switch to `axios` for DOI metadata request
    For consistency with the rest of the codebase, where we're already using
    `axios` for HTTP requests.

--- web/src/utils/doi.ts
+++ web/src/utils/doi.ts
@@ -1,9 +1,15 @@
+import axios from 'axios';
+
 export async function getDoiMetadata(doi: string, currentUrl: string): Promise<string> {
-  const prefix = currentUrl!.startsWith('https://gui-staging.dandiarchive.org/') ? 'https://handle.stage.datacite.org/' : 'https://doi.org/';
-  const url = new URL(`${prefix}${doi}`);
-  const headers = new Headers({
-    'Accept': 'application/ld+json'
+  const prefix = currentUrl!.startsWith('https://gui-staging.dandiarchive.org/')
+    ? 'https://handle.stage.datacite.org/'
+    : 'https://doi.org/';
+  const url = `${prefix}${doi}`;
+
+  const response = await axios.get(url, {
+    headers: {
+      'Accept': 'application/ld+json',
+    },
   });
-  const response = await fetch(url, { headers });
-  return await response.text();
+  return response.data;
 }
