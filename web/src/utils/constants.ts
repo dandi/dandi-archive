2e1f4a8aee13b72bf9d4cc77c049b74c98b6e858
Kabilar Gunalan <kabi@mit.edu>
2025-03-13 09:03:26 -0500

    Fix DANDI Docs links

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -1,6 +1,6 @@
 const dandiUrl = 'https://dandiarchive.org';
 const dandiAboutUrl = 'https://about.dandiarchive.org/';
-const dandiDocumentationUrl = 'https://docs.dandiarchive.org/10_using_dandi/';
+const dandiDocumentationUrl = 'https://docs.dandiarchive.org';
 const dandiHelpUrl = 'https://github.com/dandi/helpdesk/issues/new/choose';
 const dandihubUrl = 'https://hub.dandiarchive.org/';
 
