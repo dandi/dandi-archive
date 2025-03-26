cbe1ba06d919d9d5c74a815d2ba80d6b10b7c89a
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-03-26 11:04:11 -0400

    Add banner

---
+++ web/src/components/FooterBanner.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-banner color="grey darken-1 text-center">
+    <v-icon>
+      mdi-information
+    </v-icon>
+    <span>
+      This repository is under review by NIH for potential modification
+      in compliance with U.S. federal Administration directives.
+    </span>
+  </v-banner>
+</template>
