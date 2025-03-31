6bef85a6285d540605412c4de630899c7ad50d09
naglepuff <mike.nagler@kitware.com>
2025-03-31 14:26:23 -0400

    Remove puppeteer tests

--- e2e/puppeteer/jest-puppeteer.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  launch: {
-    // only use headless when not debugging
-    headless: process.env.DEBUG !== 'true',
-  },
-  // we don't want the browser to retain any session tokens after reset
-  browserContext: 'incognito',
-};
