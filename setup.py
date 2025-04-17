5a60f3f59df649c2b53e9d4f1b157c3e04924999
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2025-04-17 11:35:26 -0400

    Removed unused `django-admin-display` dependency
    This dependency is not used anywhere in the codebase.

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         # Pin dandischema to exact version to make explicit which schema version is being used
         'dandischema==0.11.0',  # schema version 0.6.9
         'django~=4.2.0',
-        'django-admin-display',
         # Pin to version where this bug is fixed
         # https://codeberg.org/allauth/django-allauth/issues/4072
         'django-allauth>=65.3.0',
