4ed0e10751089d691384dfa242fde10b6557d663
Ben Dichter <ben.dichter@gmail.com>
2025-06-04 13:31:53 -0400

    fix: update robots.txt to allow Googlebot access to dandiset metadata and disallow other bots from API endpoints

--- dandiapi/api/views/robots.py
+++ dandiapi/api/views/robots.py
@@ -4,5 +4,20 @@ from django.http import HttpResponse
 
 
 def robots_txt_view(request):
-    content = 'User-agent: *\nDisallow: /'
+    content = """# Allow Googlebot to access dandiset metadata for structured data indexing
+User-agent: Googlebot
+Allow: /api/dandisets/*/versions/*/info
+Allow: /api/dandisets/*/versions/*/
+Allow: /api/dandisets/*/info
+Allow: /api/dandisets/*/
+Allow: /api/info/
+Disallow: /api/
+
+# Disallow all other bots from accessing API endpoints
+User-agent: *
+Disallow: /api/
+Disallow: /admin/
+Disallow: /oauth/
+Disallow: /swagger/
+"""
     return HttpResponse(content, content_type='text/plain')
