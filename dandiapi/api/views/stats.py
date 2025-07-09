9871e3a97fdbd210dd01f8bd2b223c4b9448ea56
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-07-09 14:38:44 -0400

    Use ApplicationStats for /stats endpoint

--- dandiapi/api/views/stats.py
+++ dandiapi/api/views/stats.py
@@ -1,26 +1,18 @@
 from __future__ import annotations
 
-from django.contrib.auth.models import User
-from django.views.decorators.cache import cache_page
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from dandiapi.api.models import Asset, Dandiset, UserMetadata
+from dandiapi.api.models.stats import ApplicationStats
 
 
-# Cache this response for 12 hours
-@cache_page(60 * 60 * 12)
+class ApplicationStatsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplicationStats
+        exclude = ['id', 'timestamp']
+
+
 @api_view()
 def stats_view(self):
-    dandiset_count = Dandiset.objects.count()
-    published_dandiset_count = Dandiset.published_count()
-    user_count = User.objects.filter(metadata__status=UserMetadata.Status.APPROVED).count()
-    size = Asset.total_size()
-    return Response(
-        {
-            'dandiset_count': dandiset_count,
-            'published_dandiset_count': published_dandiset_count,
-            'user_count': user_count,
-            'size': size,
-        }
-    )
+    return Response(ApplicationStatsSerializer(ApplicationStats.latest()).data)
