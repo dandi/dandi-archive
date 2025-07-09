62368b4f344edab51a8175b5978ada5af2616ed0
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-07-09 14:39:16 -0400

    Remove caching configuration

--- dandiapi/settings.py
+++ dandiapi/settings.py
@@ -57,14 +57,6 @@ class DandiMixin(ConfigMixin):
             'rest_framework.authentication.TokenAuthentication',
         ]
 
-        # Caching
-        configuration.CACHES = {
-            'default': {
-                'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-                'LOCATION': 'dandi_cache_table',
-            }
-        }
-
         # Permission
         configuration.REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] += [
             'dandiapi.api.permissions.IsApprovedOrReadOnly'
@@ -168,13 +160,6 @@ class TestingConfiguration(DandiMixin, TestingBaseConfiguration):
     # Ensure celery tasks run synchronously
     CELERY_TASK_EAGER_PROPAGATES = True
     CELERY_TASK_ALWAYS_EAGER = True
-
-    # Use a dummy cache for testing
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        }
-    }
 
 
 class ProductionConfiguration(DandiMixin, ProductionBaseConfiguration):
