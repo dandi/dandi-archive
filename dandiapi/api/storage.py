ec274883d45d9d68e5a9c48b0afa779e19f34290
Yaroslav Halchenko <debian@onerussian.com>
2025-08-03 13:51:23 -0400

    Extend allowed time to fulfill PUT upload to half-hour from 10 minutes
    Both numbers are arbitrary. It could be of security hazard to make them
    arbitrarily long happen such URLs leak/get shared publicly.  But in both cases
    durations are still reasonably short.
    
    On some network(s) though we can run into situation that we do not upload "fast
    enough" and thus running into 403s which could be (I think) avoided by allowing
    for longer period of time to initiate uploads.  See
    
       https://github.com/dandi/dandi-cli/issues/1662
    
    Sure think a proper solution is TODO (as good old comment says) - for client to
    react and to re-request a new pull of URLs etc.  That is to be done, but I
    think this PR also would make a world a better place meanwhile ;)

--- dandiapi/api/storage.py
+++ dandiapi/api/storage.py
@@ -191,7 +191,7 @@ class VerbatimNameS3Storage(VerbatimNameStorageMixin, TimeoutS3Storage):
                 'ACL': 'bucket-owner-full-control',
                 'ContentMD5': base64md5,
             },
-            ExpiresIn=600,  # TODO: proper expiration
+            ExpiresIn=1800,  # TODO: proper expiration
         )
 
     def generate_presigned_head_object_url(self, key: str) -> str:
@@ -252,7 +252,7 @@ class VerbatimNameMinioStorage(VerbatimNameStorageMixin, DeconstructableMinioSto
         return self.base_url_client.presigned_put_object(
             bucket_name=self.bucket_name,
             object_name=blob_name,
-            expires=timedelta(seconds=600),  # TODO: proper expiration
+            expires=timedelta(seconds=1800),  # TODO: proper expiration
         )
 
     def generate_presigned_head_object_url(self, key: str) -> str:
