e972e1ad66ff5c4f2b0090ed763b70b368737682
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-08-28 15:25:58 -0400

    Remove version query param from schema endpoint

--- dandiapi/api/views/schema.py
+++ dandiapi/api/views/schema.py
@@ -4,13 +4,10 @@ from typing import TYPE_CHECKING
 
 from dandischema.models import Asset, Dandiset, PublishedAsset, PublishedDandiset
 from dandischema.utils import TransitionalGenerateJsonSchema
-from django.conf import settings
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import serializers, status
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from dandiapi.api.services.exceptions import DandiError
 
 if TYPE_CHECKING:
     from rest_framework.request import Request
@@ -28,7 +25,6 @@ _model_name_mapping = {
 
 
 class SchemaQuerySerializer(serializers.Serializer):
-    version = serializers.CharField(default='latest')
     model = serializers.ChoiceField(choices=list(_model_name_mapping))
 
 
@@ -38,27 +34,16 @@ class SchemaQuerySerializer(serializers.Serializer):
     operation_description='Returns the JSONSchema for supplied model metadata',
 )
 @api_view(['GET'])
-def schema_view(request: Request, version: str | None = None) -> Response:
+def schema_view(request: Request) -> Response:
     """
     Return the JSONSchema for Dandiset metadata.
 
     This endpoint provides the currently configured schema based on the application's
     DANDI_SCHEMA_VERSION setting. It can be used as a replacement for the static
     schema files hosted on GitHub.
-
-    If a version is provided and does not match the current version, a 404 is returned.
-    In the future, multiple versions could be supported.
     """
     serializer = SchemaQuerySerializer(data=request.query_params)
     serializer.is_valid(raise_exception=True)
-
-    version = serializer.validated_data['version']
-    if version and version not in {settings.DANDI_SCHEMA_VERSION, 'latest'}:
-        raise DandiError(
-            message=f'Invalid schema version {version}.'
-            f' Server schema version is {settings.DANDI_SCHEMA_VERSION}',
-            http_status_code=status.HTTP_400_BAD_REQUEST,
-        )
 
     # Generate the schema JSON using the same approach as dandischema
     model_class = _model_name_mapping[serializer.validated_data['model']]
