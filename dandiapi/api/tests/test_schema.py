7de1adb689db0a69e58e5f69e39733c2f710dfaa
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-08-28 15:25:58 -0400

    Remove version query param from schema endpoint

--- dandiapi/api/tests/test_schema.py
+++ dandiapi/api/tests/test_schema.py
@@ -2,30 +2,21 @@ from __future__ import annotations
 
 from dandischema.models import Asset, CommonModel, Dandiset, PublishedAsset, PublishedDandiset
 from dandischema.utils import TransitionalGenerateJsonSchema
-from django.conf import settings
 import pytest
 
 
 @pytest.mark.parametrize(
-    ('model', 'version'),
+    'model',
     [
-        (Dandiset, None),
-        (Asset, None),
-        (Dandiset, settings.DANDI_SCHEMA_VERSION),
-        (PublishedDandiset, None),
-        (PublishedAsset, None),
-        (PublishedDandiset, settings.DANDI_SCHEMA_VERSION),
+        Dandiset,
+        Asset,
+        PublishedDandiset,
+        PublishedAsset,
     ],
 )
-def test_schema_latest(api_client, model: CommonModel, version: str | None):
+def test_schema_latest(api_client, model: CommonModel):
     """Test that the schema endpoints return valid schemas."""
-    resp = api_client.get(
-        '/api/schema/',
-        {
-            'verison': version or '',
-            'model': model.__name__,
-        },
-    )
+    resp = api_client.get('/api/schema/', {'model': model.__name__})
     assert resp.status_code == 200
 
     # Verify that the schema is json and has core properties
@@ -37,9 +28,3 @@ def test_schema_latest(api_client, model: CommonModel, version: str | None):
     # Compare with expected schema from pydantic using same generator as dandischema
     expected_schema = model.model_json_schema(schema_generator=TransitionalGenerateJsonSchema)
     assert schema == expected_schema
-
-
-def test_schema_invalid_version(api_client):
-    """Test that schema endpoints with invalid versions return 404."""
-    resp = api_client.get('/api/schema/', {'version': 'invalid-version'})
-    assert resp.status_code == 400
