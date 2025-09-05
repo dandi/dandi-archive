4c7852782421e5f9b65837bbc4de75d8648031f5
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-09-05 14:52:17 -0400

    Rename schema get endpoint

--- dandiapi/urls.py
+++ dandiapi/urls.py
@@ -67,7 +67,7 @@ api_urlpatterns = [
     path('api/info/', info_view),
     path('api/blobs/digest/', blob_read_view, name='blob-read'),
     path('api/schema/available', schema_list_view, name='schema-list-view'),
-    path('api/schema/', schema_view, name='schema-view'),
+    path('api/schema/model', schema_view, name='schema-view'),
     path('api/uploads/initialize/', upload_initialize_view, name='upload-initialize'),
     re_path(
         r'api/uploads/(?P<upload_id>[0-9a-f\-]{36})/complete/',
