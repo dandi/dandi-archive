2a84f7bdb96c64247e50266f49a1db2624cbfacc
Brian Helba <brian.helba@kitware.com>
2025-10-15 16:06:05 -0400

    Don't suppress IntegrityError within a migration, as it's within a transaction

--- dandiapi/api/migrations/0028_default_oauth_application.py
+++ dandiapi/api/migrations/0028_default_oauth_application.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
-from contextlib import suppress
-
-from django.db import IntegrityError, migrations
+from django.db import migrations
 from oauth2_provider.settings import oauth2_settings
 
-DEFAULT_NAME = 'DANDI GUI'
 # This is specified so it matches the default value used in the frontend.
 DEFAULT_CLIENT_ID = 'Dk0zosgt1GAAKfN8LT4STJmLJXwMDPbYWYzfNtAl'
 
@@ -14,22 +11,22 @@ def create_application(apps, schema_editor):
     Application = apps.get_model(oauth2_settings.APPLICATION_MODEL)
     # This work used to be done within a now-deleted "api.0003_default_oauth_application"
     # migration, so historical databases may already have this object present; if so, this
-    # migration will silently succeed and never run again.
-    # Avoid "get_or_create" to not run an extra query every time in development.
-    with suppress(IntegrityError):
-        Application.objects.create(
-            # Production instances should change this.
-            client_id=DEFAULT_CLIENT_ID,
+    # migration will succeed and never run again.
+    Application.objects.get_or_create(
+        # Production instances should change this.
+        client_id=DEFAULT_CLIENT_ID,
+        defaults={
             # Production instances must change this.
-            redirect_uris='http://localhost:8085/',
+            'redirect_uris': 'http://localhost:8085/',
             # These values should not be modified.
-            client_type='public',
-            authorization_grant_type='authorization-code',
-            client_secret='',
-            name=DEFAULT_NAME,
+            'client_type': 'public',
+            'authorization_grant_type': 'authorization-code',
+            'client_secret': '',
+            'name': 'DANDI GUI',
             # This can be turned off in production if appropriate.
-            skip_authorization=True,
-        )
+            'skip_authorization': True,
+        },
+    )
 
 
 def reverse_create_application(apps, schema_editor):
