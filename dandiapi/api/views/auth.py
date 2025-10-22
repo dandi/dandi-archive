60ff3a39d72d1f9e1c8195ac0720e137075bac1d
Kabilar Gunalan <kabi@mit.edu>
2025-10-21 23:02:39 -0500

    Auto-approve people with Cleveland Clinc email addresses

--- dandiapi/api/views/auth.py
+++ dandiapi/api/views/auth.py
@@ -128,7 +128,13 @@ def user_questionnaire_form_view(request: AuthenticatedRequest) -> HttpResponse:
         ):
             should_auto_approve: bool = any(
                 request.user.email.endswith(suffix)
-                for suffix in ['.edu', '@alleninstitute.org', '@nih.gov', '@janelia.hhmi.org']
+                for suffix in [
+                    '.edu',
+                    '@alleninstitute.org',
+                    '@nih.gov',
+                    '@janelia.hhmi.org',
+                    '@ccf.org',
+                ]
             )
 
             # auto-approve users with edu emails, otherwise require manual approval
