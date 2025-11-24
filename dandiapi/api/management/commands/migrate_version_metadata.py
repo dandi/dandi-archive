a633c075a1b8175a8e4e473d5620e16e6cb39949
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-11-24 14:12:16 -0500

    Update management command for schema migration

--- dandiapi/api/management/commands/migrate_version_metadata.py
+++ dandiapi/api/management/commands/migrate_version_metadata.py
@@ -1,28 +1,92 @@
 from __future__ import annotations
 
-from dandischema import migrate
+import logging
+
+from dandischema.metadata import migrate
+from django.db import transaction
 import djclick as click
 
 from dandiapi.api.models import Version
+from dandiapi.api.services import audit
+from dandiapi.api.services.metadata import validate_version_metadata
+
+logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.argument('to_version')
-def migrate_version_metadata(*, to_version: str):
-    click.echo(f'Migrating all version metadata to version {to_version}')
-    for version in Version.objects.filter(version='draft'):
-        click.echo(f'Migrating {version.dandiset.identifier}/{version.version}')
+@click.argument(
+    'dandisets',
+    type=click.INT,
+    nargs=-1,
+)
+@click.option(
+    '--target', 'target_version', default='', help='The target dandischema version to migrate to.'
+)
+@click.option(
+    '-a',
+    '--all',
+    'include_all',
+    is_flag=True,
+    help='Run on all dandisets.',
+)
+def migrate_version_metadata(dandisets: tuple[int, ...], *, include_all: bool, target_version: str):
+    if bool(dandisets) == include_all:
+        raise click.ClickException("Must specify exactly one of 'dandisets' or --all")
+    if not target_version:
+        raise click.ClickException('Must specify target schema version via --target')
 
-        metadata = version.metadata
+    versions = Version.objects.filter(version='draft')
+    if dandisets:
+        versions = versions.filter(dandiset_id__in=dandisets)
 
-        try:
-            metanew = migrate(metadata, to_version=to_version, skip_validation=True)
-        except Exception as e:  # noqa: BLE001
-            click.echo(f'Failed to migrate {version.dandiset.identifier}/{version.version}')
-            click.echo(e)
-            continue
+    logger.info(
+        'Migrating %s dandiset draft versions to schema version %s',
+        versions.count(),
+        target_version,
+    )
 
-        if version.metadata != metanew:
+    migrated_count = 0
+    for version in versions.iterator():
+        logger.info('-----------------------------------------')
+        logger.info('Migrating %s', version)
+
+        with transaction.atomic():
+            locked_version = Version.objects.select_for_update().get(id=version.id)
+
+            try:
+                metanew = migrate(
+                    locked_version.metadata, to_version=target_version, skip_validation=True
+                )
+                # We set this manually due to https://github.com/dandi/dandi-schema/issues/353
+                metanew['schemaVersion'] = target_version
+            except Exception as e:
+                logger.exception('Failed to migrate %s', version, exc_info=e)
+                continue
+
+            if locked_version.metadata == metanew:
+                logger.info('No change in metadata for %s. Skipping save...', version)
+                continue
+
             version.metadata = metanew
             version.status = Version.Status.PENDING
             version.save()
+
+            audit.update_metadata(
+                dandiset=locked_version.dandiset,
+                metadata=locked_version.metadata,
+                user=None,
+                admin=True,
+                description=f'Update schema version to {target_version}',
+            )
+
+            migrated_count += 1
+            logger.info('Metadata migrated for version %s', version)
+
+        # Validate outside of transaction, since this function uses `select_for_update` itself
+        validate_version_metadata(version=version)
+
+    logger.info(
+        'Migrated %s out of %s total selected versions',
+        migrated_count,
+        versions.count(),
+    )
