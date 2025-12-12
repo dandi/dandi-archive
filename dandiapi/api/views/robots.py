f5cb4b03803bb592385b23700e16555d92c36017
Roni Choudhury <roni.choudhury@kitware.com>
2025-12-12 11:28:33 -0500

    Add sitemap to robots.txt

--- dandiapi/api/views/robots.py
+++ dandiapi/api/views/robots.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+from django.conf import settings
 from django.http import HttpResponse
 
 
 def robots_txt_view(request):
-    content = """# Allow Googlebot to access dandiset metadata for structured data indexing
+    content = f"""# Allow Googlebot to access dandiset metadata for structured data indexing
 User-agent: Googlebot
 Allow: /api/dandisets/search
 Allow: /api/dandisets/*/
@@ -17,5 +18,8 @@ Disallow: /
 # Disallow all other bots from accessing API endpoints
 User-agent: *
 Disallow: /
+
+# Sitemap location
+Sitemap: {settings.DANDI_API_URL}/sitemap.xml
 """
     return HttpResponse(content, content_type='text/plain')
