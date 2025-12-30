fbea4803994eb5e1db0aa252dba46a61a09a6a51
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-12-30 14:28:46 -0500

    Ensure contact person is present on new dandisets

--- dandiapi/api/services/version/metadata.py
+++ dandiapi/api/services/version/metadata.py
@@ -6,6 +6,71 @@ from dandischema.consts import DANDI_SCHEMA_VERSION
 
 from dandiapi.api.models.version import Version
 
+CONTACT_PERSON_ROLE = 'dcite:ContactPerson'
+
+
+def _normalize_contributor(version_metadata: dict, name: str, email: str) -> dict:
+    """
+    Modify `version_metadata` to ensure the contributor field is the correct shape.
+
+    This function ensures that the contributor field is properly formed, and that there is an
+    entry in this field with the role of contact person. This field is used in other parts of the
+    codebase, so it's important to process this.
+    """
+    default_contributor = {
+        'name': name,
+        'email': email,
+        'roleName': [CONTACT_PERSON_ROLE],
+        'schemaKey': 'Person',
+        'affiliation': [],
+        'includeInCitation': True,
+    }
+
+    # Ensure that the contributor field is of the correct shape
+    contributor = version_metadata.get('contributor')
+    if not isinstance(contributor, list):
+        version_metadata['contributor'] = [default_contributor]
+        return version_metadata
+
+    # We know contributor field is a list, but the values could be anything.
+    # Filter the value to be only dicts.
+    version_metadata['contributor'] = [cont for cont in contributor if isinstance(cont, dict)]
+
+    contributor = version_metadata['contributor']
+
+    # Check that no contact person exists. If they do, we don't need to do anything
+    contact_person = next(
+        (
+            cont
+            for cont in contributor
+            if isinstance(cont.get('roleName'), list)
+            and CONTACT_PERSON_ROLE in cont.get('roleName', [])
+        ),
+        None,
+    )
+    if contact_person:
+        return version_metadata
+
+    # No contact person exists, check if author exists. If not, we will set the default contributor
+    author_contributor = next(
+        (cont for cont in contributor if cont.get('name') == name and cont.get('email') == email),
+        None,
+    )
+    if not author_contributor:
+        contributor.append(default_contributor)
+        return version_metadata
+
+    # Ensure the contact person role exists in the author contributor
+    roles = author_contributor.get('roleName')
+    if not isinstance(roles, list):
+        author_contributor['roleName'] = [CONTACT_PERSON_ROLE]
+        return version_metadata
+
+    if CONTACT_PERSON_ROLE not in roles:
+        roles.append(CONTACT_PERSON_ROLE)
+
+    return version_metadata
+
 
 def _normalize_version_metadata(raw_version_metadata: dict, name: str, email: str) -> dict:
     """
@@ -16,20 +81,8 @@ def _normalize_version_metadata(raw_version_metadata: dict, name: str, email: st
     # Strip away any computed fields
     version_metadata = copy.deepcopy(Version.strip_metadata(raw_version_metadata))
 
-    # Only inject a schemaVersion and default contributor field if they are
-    # not specified in the version_metadata
-    return {
-        'schemaKey': 'Dandiset',
-        'schemaVersion': DANDI_SCHEMA_VERSION,
-        'contributor': [
-            {
-                'name': name,
-                'email': email,
-                'roleName': ['dcite:ContactPerson'],
-                'schemaKey': 'Person',
-                'affiliation': [],
-                'includeInCitation': True,
-            },
-        ],
-        **version_metadata,
-    }
+    version_metadata['schemaKey'] = 'Dandiset'
+    version_metadata.setdefault('schemaVersion', DANDI_SCHEMA_VERSION)
+    _normalize_contributor(version_metadata, name, email)
+
+    return version_metadata
