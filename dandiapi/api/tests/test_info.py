57f7e5bfde36b06427d49b23a531489e01b710fd
Jacob Nesbitt <jjnesbitt2@gmail.com>
2026-02-19 11:59:16 -0500

    Add test to ensure cli-minimal-version is accurate

--- dandiapi/api/tests/test_info.py
+++ dandiapi/api/tests/test_info.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import importlib.metadata
+
 from dandischema.conf import get_instance_config
+from packaging.specifiers import SpecifierSet
 import pytest
+import requests
 
 
 @pytest.mark.django_db
@@ -16,3 +20,22 @@ def test_rest_info_instance_config_include_none(api_client):
         resp.json()['instance_config'].keys()
         == get_instance_config().model_dump(mode='json', exclude_none=False).keys()
     )
+
+
+def test_cli_minimal_version_matches_dandischema(api_client):
+    """Test that local dandischema and minimal cli dandischema are compatible."""
+    # Get CLI version info from pypi
+    minimal_version = api_client.get('/api/info/').json()['cli-minimal-version']
+    data = requests.get(f'https://pypi.org/pypi/dandi/{minimal_version}/json').json()
+
+    # Extract the dandischema requirement
+    dandischema_requires = [x for x in data['info']['requires_dist'] if x.startswith('dandischema')]
+    assert len(dandischema_requires) == 1
+    dandischema_version: str = dandischema_requires[0]
+    version_range = SpecifierSet(
+        dandischema_version.split(';', maxsplit=1)[0].removeprefix('dandischema').strip()
+    )
+
+    # Ensure that local dandischema is compatible with the CLI version
+    local_version = importlib.metadata.version('dandischema')
+    assert version_range.contains(local_version)
