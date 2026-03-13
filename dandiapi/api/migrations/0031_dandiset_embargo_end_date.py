8862f288f7f1271053f0054fa7be3460487a8e05
Jacob Nesbitt <jjnesbitt2@gmail.com>
2026-03-13 17:53:44 -0400

    Add Dandiset.embargo_end_date

---
+++ dandiapi/api/migrations/0031_dandiset_embargo_end_date.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from datetime import date
+
+from django.conf import settings
+from django.db import migrations, models
+from tqdm import tqdm
+
+
+def populate_embargo_end_date(apps, schema_editor):
+    Dandiset = apps.get_model('api', 'Dandiset')
+    Version = apps.get_model('api', 'Version')
+
+    embargoed_dandisets = Dandiset.objects.filter(
+        embargo_status='EMBARGOED', versions__metadata__access__0__embargoedUntil__isnull=False
+    )
+    for dandiset in tqdm(embargoed_dandisets.iterator(), total=embargoed_dandisets.count()):
+        draft_version = Version.objects.get(dandiset=dandiset, version='draft')
+
+        # embargoedUntil is stored as an ISO 8601 date string, e.g. "2025-01-01"
+        embargoed_until = draft_version.metadata['access'][0]['embargoedUntil']
+
+        try:
+            dandiset.embargo_end_date = date.fromisoformat(embargoed_until)
+            dandiset.save(update_fields=['embargo_end_date'])
+        except (TypeError, ValueError):
+            pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('api', '0030_alter_asset_path'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='dandiset',
+            name='embargo_end_date',
+            field=models.DateField(blank=True, default=None, null=True),
+        ),
+        migrations.RunPython(populate_embargo_end_date, migrations.RunPython.noop),
+        migrations.AddConstraint(
+            model_name='dandiset',
+            constraint=models.CheckConstraint(
+                condition=models.Q(
+                    ('embargo_end_date__isnull', False), ('embargo_status', 'OPEN'), _connector='OR'
+                ),
+                name='embargoed_dandiset_has_embargo_end_date',
+            ),
+        ),
+    ]
